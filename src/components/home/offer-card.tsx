9d7db7dc54a6472990185bf9d6e871cc46f643d8
Toby2507 <oluwatobisalau2000@gmail.com>
2024-05-30 17:10:29 +0100

    FIX: minor import and semantic syntax fixes

--- src/components/home/offer-card.tsx
+++ src/components/home/offer-card.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { paths } from '@/utils';
-import { IOffers } from '@/utils/interfaces';
+import { IOffers } from '@/types';
 import { Button, Card, CardBody } from '@nextui-org/react';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -10,7 +10,7 @@ const OfferCard = ({ id, name, description, imgUrl, link }: IOffers) => {
   return (
     <Card radius="sm">
       <CardBody className="grid grid-cols-2 gap-6 p-2">
-        <Image src={imgUrl} alt={name} className="h-full w-full rounded-s-md" />
+        <Image src={imgUrl} alt={name} className="h-full object-cover rounded-s-md" />
         <div className="flex flex-col justify-between gap-6 py-3">
           <div className="flex flex-col gap-1">
             <h3 className="text-lg font-bold">{name}</h3>
--- src/components/home/place-card.tsx
+++ src/components/home/place-card.tsx
@@ -1,34 +1,32 @@
 import { paths } from '@/utils';
-import { IOffers } from '@/utils/interfaces';
+import { IOffers } from '@/types';
 import { Card, CardBody, CardFooter, Image } from '@nextui-org/react';
 import Link from 'next/link';
 
 const PlaceCard = ({ id, name, description, imgUrl }: IOffers) => {
   return (
-    <article className="basis-1/3 px-2 py-2">
-      <Card shadow="none" className="grid grid-rows-2">
-        <CardBody className="p-0">
-          <Image
-            src={imgUrl.src}
-            alt={name}
-            width="full"
-            height={224}
-            radius="none"
-            isZoomed
-            className="h-56 w-full object-cover"
-          />
-        </CardBody>
-        <CardFooter className="flex flex-col items-start justify-between border border-accentGray2/50 px-6 pb-6">
-          <div className="flex flex-col gap-1">
-            <h3 className="text-xl font-bold">{name}</h3>
-            <p className="text-xs font-normal">{description}</p>
-          </div>
-          <Link href={paths.details(id)}>
-            <p className="text-xs text-primary font-semibold hover:underline">See more {' >'}</p>
-          </Link>
-        </CardFooter>
-      </Card>
-    </article>
+    <Card shadow="none" className="grid grid-rows-2">
+      <CardBody className="p-0">
+        <Image
+          src={imgUrl.src}
+          width="full"
+          alt={name}
+          height={224}
+          radius="none"
+          isZoomed
+          className="h-56 w-full object-cover"
+        />
+      </CardBody>
+      <CardFooter className="flex flex-col items-start justify-between border border-accentGray2/50 px-6 pb-6">
+        <div className="flex flex-col gap-1">
+          <h3 className="text-xl font-bold">{name}</h3>
+          <p className="text-xs font-normal">{description}</p>
+        </div>
+        <Link href={paths.details(id)}>
+          <p className="text-xs text-primary font-semibold hover:underline">See more {' >'}</p>
+        </Link>
+      </CardFooter>
+    </Card>
   );
 };
 
