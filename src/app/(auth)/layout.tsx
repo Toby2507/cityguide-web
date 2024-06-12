2ee43c942e9c5b8ea9211d00e7f81a01d4ed4939
Toby2507 <oluwatobisalau2000@gmail.com>
2024-06-12 05:48:19 +0100

    Convert the logo to a link to the home page

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,6 @@
 import loginBanner from '@images/login-banner.png';
 import Image from 'next/image';
+import Link from 'next/link';
 
 interface ILayout {
   children: React.ReactNode;
@@ -9,7 +10,9 @@ const Layout = ({ children }: Readonly<ILayout>) => {
   return (
     <div className="grid grid-cols-2 max-h-screen">
       <div className="flex flex-col gap-14 p-20">
-        <h1 className="text-4xl font-bold text-primary">CityGuideX</h1>
+        <Link href="/">
+          <h1 className="text-4xl font-bold text-primary cursor-pointer">CityGuideX</h1>
+        </Link>
         {children}
       </div>
       <Image className="w-full max-h-screen object-cover" alt="beatiful resort with sky blue pool" src={loginBanner} />
