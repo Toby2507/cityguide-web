cb621dcd3d21fe568e8cbf2879723957d87abdbd
Toby2507 <oluwatobisalau2000@gmail.com>
2024-06-15 10:01:02 +0100

    Add the create establishment schema for input validation in the server action for creating establishmentsF

--- src/utils/schemas.ts
+++ src/utils/schemas.ts
@@ -1,4 +1,4 @@
-import { object, string } from 'zod';
+import { number, object, string } from 'zod';
 
 export const createUserSchema = object({
   firstName: string({ required_error: 'First name is required' }).min(3, 'First name requires atleast 3 characters'),
@@ -11,6 +11,36 @@ export const createUserSchema = object({
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,24}$/,
     '8 to 24 characters. Must include uppercase and lowercase letters, a number and a special character. Allowed special characters: ! @ # $ % ^ & *'
   ),
+});
+
+export const createEstablishmentSchema = object({
+  name: string({ required_error: 'Establishment Name is required' }).min(
+    3,
+    'Establishment name requires atleast 3 characters'
+  ),
+  description: string().optional(),
+  address: object({
+    name: string({ required_error: 'Address name is required' }),
+    fullAddress: string().optional(),
+    locationId: string({ required_error: 'Address location id is required' }),
+    city: string().optional(),
+    state: string({ required_error: 'State is required' }),
+    country: string({ required_error: 'Country is required' }),
+    geoLocation: object({
+      lat: number({
+        required_error: 'Latitude is required',
+        invalid_type_error: 'Latitude has to be a number',
+      }),
+      lng: number({
+        required_error: 'Longitude is required',
+        invalid_type_error: 'Longitude has to be a number',
+      }),
+    }),
+    extraDetails: string().optional(),
+  }),
+  phoneNumber: string({ required_error: 'Phone number is required' }).min(11, 'Invalid phone number'),
+  email: string({ required_error: 'Email is required' }).email('Invalid email'),
+  password: string({ required_error: 'Password is required' }).min(8, 'Password should be atleast 8 characters'),
 });
 
 export const loginUserSchema = object({
