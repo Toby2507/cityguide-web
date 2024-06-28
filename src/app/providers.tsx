744a078785e5e4684232dc8e208499de192db751
Toby2507 <oluwatobisalau2000@gmail.com>
2024-06-28 08:56:35 +0100

    Add the reservation store provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { ReservationStoreProvider } from '@/providers';
 import { NextUIProvider } from '@nextui-org/react';
 import { SessionProvider } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
@@ -12,7 +13,9 @@ const Providers = ({ children }: IProviders) => {
   const router = useRouter();
   return (
     <SessionProvider>
-      <NextUIProvider navigate={router.push}>{children}</NextUIProvider>
+      <ReservationStoreProvider>
+        <NextUIProvider navigate={router.push}>{children}</NextUIProvider>
+      </ReservationStoreProvider>
     </SessionProvider>
   );
 };
