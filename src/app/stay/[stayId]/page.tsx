e5f16de20ec0112b56dac954ae18ce872fa130bd
Toby2507 <oluwatobisalau2000@gmail.com>
2024-06-28 08:57:32 +0100

    Add the footer and subscribe box to the stay detail page and add a background color of white as a result of the blue of the html element

--- src/app/stay/[stayId]/page.tsx
+++ src/app/stay/[stayId]/page.tsx
@@ -1,5 +1,13 @@
 import { StayDetailNav } from '@/components';
-import { Header, StayDetailAmenities, StayDetailAvailability, StayDetailOverview, StaySearchBar } from '@/containers';
+import {
+  Footer,
+  Header,
+  StayDetailAmenities,
+  StayDetailAvailability,
+  StayDetailOverview,
+  StaySearchBar,
+  SubscribeBox,
+} from '@/containers';
 
 interface IStayDetailPage {
   params: {
@@ -11,13 +19,17 @@ const StayDetailPage = ({ params: { stayId } }: IStayDetailPage) => {
   return (
     <>
       <Header />
-      <main className="container mx-auto px-10 flex flex-col gap-6 max-w-7xl">
-        <StaySearchBar />
-        <StayDetailNav />
-        <StayDetailOverview />
-        <StayDetailAmenities />
-        <StayDetailAvailability />
+      <main className="flex flex-col gap-20 bg-white">
+        <div className="container mx-auto px-10 flex flex-col gap-6 max-w-7xl">
+          <StaySearchBar />
+          <StayDetailNav />
+          <StayDetailOverview />
+          <StayDetailAmenities />
+          <StayDetailAvailability />
+        </div>
+        <SubscribeBox />
       </main>
+      <Footer />
     </>
   );
 };
