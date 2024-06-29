0da77bdf65c8d0c3e7b7ca658749fa928218d592
Toby2507 <oluwatobisalau2000@gmail.com>
2024-06-29 18:30:50 +0100

    Refactor the getUser function and the logout function and add the new setCookies function

--- src/server/queries/auth.ts
+++ src/server/queries/auth.ts
@@ -1,14 +1,32 @@
 'use server';
 
+import { IUserDetails } from '@/types';
 import { cookies } from 'next/headers';
 
 export const getUser = async () => {
-  const user = cookies().get('city-guide-user');
+  const fullName = cookies().get('city-guide-username')?.value!;
+  const imgUrl = cookies().get('city-guide-userimg')?.value!;
+  const id = cookies().get('city-guide-userid')?.value!;
+  const type = cookies().get('city-guide-type')?.value! as 'establishment' | 'user';
+  const user: IUserDetails = { id, fullName, imgUrl, type };
   return user;
 };
 
+export const setCookies = (res: any, type: 'user' | 'establishment') => {
+  cookies().set('token', res.accessToken);
+  cookies().set(
+    'city-guide-username',
+    type === 'user' ? `${res.user.firstName} ${res.user.lastName}` : res.establishment.name
+  );
+  cookies().set('city-guide-userimg', type === 'user' ? res.user.imgUrl : res.establishment.imgUrl);
+  cookies().set('city-guide-userid', type === 'user' ? res.user._id : res.establishment._id);
+  cookies().set('city-guide-type', type);
+};
+
 const _logout = async () => {
-  cookies().delete('city-guide-user');
+  cookies().delete('city-guide-username');
+  cookies().delete('city-guide-userimg');
+  cookies().delete('city-guide-userid');
   cookies().delete('token');
 };
 
