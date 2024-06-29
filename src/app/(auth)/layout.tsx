0947a8be620be4922e1e2ea1dcfdb45e818f97bd
Toby2507 <oluwatobisalau2000@gmail.com>
2024-06-29 18:23:41 +0100

    Add a white background to the layout screen of the auth pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -8,7 +8,7 @@ interface ILayout {
 
 const Layout = ({ children }: Readonly<ILayout>) => {
   return (
-    <div className="grid grid-cols-2 max-h-screen overflow-y-hidden">
+    <div className="grid grid-cols-2 max-h-screen overflow-y-hidden bg-white">
       <div className="flex flex-col gap-14 p-20 h-screen overflow-y-auto">
         <Link href="/">
           <h1 className="text-4xl font-bold text-primary cursor-pointer">CityGuideX</h1>
