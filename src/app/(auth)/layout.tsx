2e10dc65d32051da98085b689de0a5d3eb6b6ddc
Toby2507 <oluwatobisalau2000@gmail.com>
2024-07-02 08:49:31 +0100

    Replace div with the more semantic main element

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -9,12 +9,12 @@ interface ILayout {
 const Layout = ({ children }: Readonly<ILayout>) => {
   return (
     <div className="grid grid-cols-2 max-h-screen overflow-y-hidden bg-white">
-      <div className="flex flex-col gap-14 p-20 h-screen overflow-y-auto">
+      <main className="flex flex-col gap-14 p-20 h-screen overflow-y-auto">
         <Link href="/">
           <h1 className="text-4xl font-bold text-primary cursor-pointer">CityGuideX</h1>
         </Link>
         {children}
-      </div>
+      </main>
       <Image className="w-full max-h-screen object-cover" alt="beatiful resort with sky blue pool" src={loginBanner} />
     </div>
   );
