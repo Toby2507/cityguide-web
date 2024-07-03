3dd55dc3d20558b55f82b1101d188ae8ad351271
Toby2507 <oluwatobisalau2000@gmail.com>
2024-07-03 13:28:10 +0100

    Add the dashboard chart box

--- src/containers/admin/dashboard.tsx
+++ src/containers/admin/dashboard.tsx
@@ -1,5 +1,5 @@
-import { MetricCard } from '@/components';
-import { metrics } from '@/data';
+import { DashboardChart, EngagementCard, MetricCard } from '@/components';
+import { engagements, metrics } from '@/data';
 
 const AdminDashboard = () => {
   const tMetrics = metrics.map((metric) => {
@@ -11,12 +11,40 @@ const AdminDashboard = () => {
     }
   });
   return (
-    <section id="dashboard">
+    <section className="flex flex-col gap-10" id="dashboard">
       {/* <!-- Metrics --> */}
       <div className="flex items-center gap-4">
         {tMetrics.map((metric) => (
           <MetricCard key={metric.id} {...metric} />
         ))}
+      </div>
+      <div className="flex items-center gap-4">
+        <article className="flex flex-col shadow-xl">
+          <div className="flex flex-col gap-6 px-6 py-4">
+            <p className="text-sm text-accentGray font-semibold">Recent Engagements</p>
+            <div className="grid grid-cols-2 gap-4">
+              {engagements.map((eng) => (
+                <EngagementCard key={eng.id} {...eng} />
+              ))}
+            </div>
+          </div>
+          <div className="flex flex-col -mt-2">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+              <path
+                fill="#AED4FF"
+                fill-opacity="1"
+                d="M0,192L120,170.7C240,149,480,107,720,106.7C960,107,1200,149,1320,170.7L1440,192L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"
+              ></path>
+            </svg>
+            <button className="text-white text-sm tracking-wide bg-accentLightBlue -mt-10 rounded-t-2xl pb-2">
+              View Details
+            </button>
+          </div>
+        </article>
+        <figure className="flex-1 h-full shadow-xl px-6 py-4">
+          <p className="text-sm text-accentGray font-semibold">Reservations</p>
+          <DashboardChart />
+        </figure>
       </div>
     </section>
   );
