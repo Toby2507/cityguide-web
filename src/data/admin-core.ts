73193adfafda7d0a2dce971069240bc03229049a
Toby2507 <oluwatobisalau2000@gmail.com>
2024-07-03 13:28:37 +0100

    Add demo data for dashboard chart

--- src/data/admin-core.ts
+++ src/data/admin-core.ts
@@ -6,6 +6,7 @@ import { IoBedOutline, IoLogOutOutline, IoRestaurantOutline, IoSettingsOutline }
 import { MdOutlineRateReview, MdPendingActions, MdFreeCancellation } from 'react-icons/md';
 import { HiMiniSquare3Stack3D } from 'react-icons/hi2';
 import { PiWine, PiStackPlusFill } from 'react-icons/pi';
+import { EngagementType, IEngagement } from '@/types';
 
 export const adminNav = [
   { title: 'Dashboard', href: '/admin', Icon: GoHome },
@@ -27,4 +28,94 @@ export const metrics = [
   { id: 'totalRes', title: 'Total Reservations', value: 0, href: '/admin', Icon: BsListCheck },
   { id: 'pendingRes', title: 'Pending Reservations', value: 0, href: '/admin', Icon: MdPendingActions },
   { id: 'cancelledRes', title: 'Cancelled Reservations', value: 0, href: '/admin', Icon: MdFreeCancellation },
+];
+
+export const engagements: IEngagement[] = [
+  {
+    id: '1',
+    imgUrl: 'https://www.w3schools.com/howto/img_avatar2.png',
+    name: 'John Doe',
+    type: EngagementType.NEW,
+    href: '/admin',
+  },
+  {
+    id: '2',
+    imgUrl: 'https://www.w3schools.com/howto/img_avatar2.png',
+    name: 'John Doe',
+    type: EngagementType.CANCELLED,
+    href: '/admin',
+  },
+  {
+    id: '3',
+    imgUrl: 'https://www.w3schools.com/howto/img_avatar2.png',
+    name: 'John Doe',
+    type: EngagementType.NEW,
+    href: '/admin',
+  },
+  {
+    id: '4',
+    imgUrl: 'https://www.w3schools.com/howto/img_avatar2.png',
+    name: 'John Doe',
+    type: EngagementType.REVIEW,
+    href: '/admin',
+  },
+  {
+    id: '5',
+    imgUrl: 'https://www.w3schools.com/howto/img_avatar2.png',
+    name: 'John Doe',
+    type: EngagementType.NEW,
+    href: '/admin',
+  },
+  {
+    id: '6',
+    imgUrl: 'https://www.w3schools.com/howto/img_avatar2.png',
+    name: 'John Doe',
+    type: EngagementType.REVIEW,
+    href: '/admin',
+  },
+];
+
+export const chartData = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
 ];
