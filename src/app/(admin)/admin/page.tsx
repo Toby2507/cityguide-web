d5bbe7fead4d4dc4748d48d143303982df96b5ed
Toby2507 <oluwatobisalau2000@gmail.com>
2024-07-06 08:23:11 +0100

    Add the admin properties and reservation sections

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,9 +1,11 @@
-import { AdminDashboard } from '@/containers';
+import { AdminDashboard, AdminProperties, AdminReservation } from '@/containers';
 
 const AdminDashboardPage = () => {
   return (
-    <div>
+    <div className="flex flex-col gap-6 pb-20">
       <AdminDashboard />
+      <AdminProperties />
+      <AdminReservation />
     </div>
   );
 };
