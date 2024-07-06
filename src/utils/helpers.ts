80a00a9330b759ef924ab70ea7ed732ed0f9fcee
Toby2507 <oluwatobisalau2000@gmail.com>
2024-07-06 08:47:44 +0100

    Add the format number and currency functions

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -11,3 +11,16 @@ export const addressFormatter = (res: google.maps.places.PlaceResult): IAddress 
     geoLocation: { lat: res.geometry?.location?.lat()!, lng: res.geometry?.location?.lng()! },
   };
 };
+
+export const numberFormat = (number: number) => {
+  number = number || 0;
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
+
+export const numberToCurrency = (value: number, prefix: boolean = false) => {
+  const number = (value || 0).toFixed(2);
+  const [currency, decimal] = number.split('.');
+  if (prefix && value < 1000000) return `₦${Math.ceil(value / 1000)}K`;
+  if (prefix && value >= 1000000) return `₦${Math.ceil(value / 1000000)}M`;
+  return `₦${numberFormat(+currency)}.${decimal}`;
+};
