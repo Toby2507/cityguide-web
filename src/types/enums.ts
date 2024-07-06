b25b1a5561156b8d156127b3c382a505c7aac022
Toby2507 <oluwatobisalau2000@gmail.com>
2024-07-06 08:46:51 +0100

    Update the model types and enums to be on par with the server

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -3,12 +3,41 @@ export enum Roles {
   User = 'user',
 }
 
-export enum Stays {
-  Hotel = 'hotel',
-  Apartment = 'apartment',
-  Hostel = 'hostel',
-  Resort = 'resort',
-  Bnb = 'bnb',
+export enum PropertyType {
+  STAY = 'Stay',
+  RESTAURANT = 'Restaurant',
+  NIGHTLIFE = 'NightLife',
+}
+
+export enum StayType {
+  HOTEL = 'Hotel',
+  HOSTEL = 'Hostel',
+  RESORT = 'Resort',
+  APARTMENT = 'Apartment',
+  BnB = 'BnB',
+}
+
+export enum NightLifeType {
+  CLUB = 'Club',
+  BAR = 'Bar',
+  LOUNGE = 'Lounge',
+  OTHER = 'Other',
+}
+
+export enum Status {
+  PENDING = 'Pending',
+  CONFIRMED = 'Confirmed',
+  CANCELLED = 'Cancelled',
+  COMPLETED = 'Completed',
+}
+
+export enum Rating {
+  NO_RATING,
+  ONE,
+  TWO,
+  THREE,
+  FOUR,
+  FIVE,
 }
 
 export enum Parking {
@@ -17,10 +46,27 @@ export enum Parking {
   NO = 'No',
 }
 
-export enum PropertyType {
-  STAY = 'Stay',
-  RESTAURANT = 'Restaurant',
-  NIGHTLIFE = 'NightLife',
+export enum MaxDays {
+  DEFAULT = 30,
+  QUARTER = 45,
+  HALF = 60,
+  FULL = 90,
+}
+
+export enum PriceRange {
+  BUDGET = 'Budget-friendly',
+  MODERATE = 'Mid-range',
+  FINE = 'Fine-dining',
+}
+
+export enum DayOfWeek {
+  MONDAY = 'Monday',
+  TUESDAY = 'Tuesday',
+  WEDNESDAY = 'Wednesday',
+  THURSDAY = 'Thursday',
+  FRIDAY = 'Friday',
+  SATURDAY = 'Saturday',
+  SUNDAY = 'Sunday',
 }
 
 export enum EngagementType {
--- src/types/model.interfaces.ts
+++ src/types/model.interfaces.ts
@@ -1,5 +1,32 @@
 import { LatLng } from './common.interfaces';
-import { Parking, PropertyType } from './enums';
+import {
+  DayOfWeek,
+  MaxDays,
+  NightLifeType,
+  Parking,
+  PriceRange,
+  PropertyType,
+  Rating,
+  Status,
+  StayType,
+} from './enums';
+
+// General
+export interface ISocialLink {
+  name: string;
+  handle: string;
+}
+
+export interface IContact {
+  email: string;
+  phone?: string;
+  socialMedia?: ISocialLink[];
+}
+
+export interface IGuests {
+  adults: number;
+  children: number;
+}
 
 export interface IAddress {
   name: string;
@@ -12,9 +39,120 @@ export interface IAddress {
   extraDetails?: string;
 }
 
+export interface ICustomAvailability {
+  day: DayOfWeek;
+  from: string;
+  to: string;
+}
+
+export interface IAvailability {
+  type: 'ANYTIME' | 'CUSTOM';
+  custom: ICustomAvailability[];
+}
+
+// NightLife
+export interface IEvent {
+  id: string;
+  name: string;
+  description: string;
+  dates: { from: string; to: string }[];
+  specialGuests: string[];
+  price: number;
+  imgUrl: string;
+  availableTickets: number;
+}
+
+export interface INightLifeRules {
+  minAge: number;
+  parking: Parking;
+  dressCode?: string[];
+  musicGenre?: string[];
+}
+
+export interface INightLifeDetails {
+  entryFee?: number;
+  paymentOptions: string[];
+  amenities: string[];
+}
+
+export interface INightLife {
+  _id: string;
+  establishment: string;
+  type: NightLifeType;
+  name: string;
+  summary: string;
+  description?: string;
+  address: IAddress;
+  rating: Rating;
+  avatar: string;
+  images?: string[];
+  availability: ICustomAvailability[];
+  rules: INightLifeRules;
+  details: INightLifeDetails;
+  contact: IContact;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+// Restaurants
+export interface IMenu {
+  id: string;
+  name: string;
+  description: string;
+  imgUrl: string;
+  price?: number;
+  category?: string[];
+  dietaryRestrictions?: string[];
+}
+
+export interface IRestaurantDetails {
+  delivery: boolean;
+  reservation?: number;
+  amenities?: string[];
+  paymentOptions?: string[];
+  children: boolean;
+}
+
+export interface IRestaurant {
+  _id: string;
+  establishment: string;
+  name: string;
+  summary: string;
+  description?: string;
+  address: IAddress;
+  rating: Rating;
+  avatar: string;
+  images?: string[];
+  availability: ICustomAvailability[];
+  priceRange: PriceRange;
+  serviceStyle?: string[];
+  cuisine?: string[];
+  dietaryProvisions?: string[];
+  menu: IMenu[];
+  details: IRestaurantDetails;
+  contact: IContact;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+// Stays
 export interface IBed {
   type: string;
   count: number;
+}
+
+export interface IStayRules {
+  checkIn: string;
+  checkOut: string;
+  smoking: boolean;
+  pets: boolean;
+  parties: boolean;
+}
+
+export interface IExtraInfo {
+  host?: { name: string; info: string };
+  property?: string;
+  neighbourhood?: string;
 }
 
 export interface IAccommodation {
@@ -35,8 +173,40 @@ export interface IAccommodation {
   price: number;
 }
 
+export interface IStay {
+  _id: string;
+  partner: string;
+  partnerType: string;
+  type: StayType;
+  name: string;
+  summary: string;
+  extraInfo?: IExtraInfo;
+  address: IAddress;
+  rating: number;
+  avatar: string;
+  images?: string[];
+  amenities: string[];
+  hotelRating?: Rating;
+  rules: IStayRules;
+  accommodation: IAccommodation[];
+  maxDays: MaxDays;
+  language: string[];
+  createdAt: string;
+  updatedAt: string;
+}
+
+// Reservations
 export interface IReservation {
+  _id: string;
   property: string;
+  user: {
+    _id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    phoneNumber: string;
+    imgUrl: string | null;
+  };
   propertyType: PropertyType;
   checkInDay: string;
   checkInTime: string;
@@ -44,6 +214,9 @@ export interface IReservation {
   checkOutTime: string;
   roomId?: string;
   reservationCount: number;
-  noOfGuests: { adults: number; children: number };
+  status: Status;
+  noOfGuests: IGuests;
   price?: number;
+  createdAt: string;
+  updatedAt: string;
 }
