6d679bfd570a8d15f00b938c11841f8119062500
Toby2507 <oluwatobisalau2000@gmail.com>
2024-07-06 08:46:16 +0100

    Add a new data file to house demo model data to better reflect what is to be expected when linking to the backend

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,5 +1,6 @@
 export * from './admin-core';
 export * from './amenities';
 export * from './demo-locations';
+export * from './demo-models';
 export * from './demo-services';
 export * from './support';
