663308822a858124aab8bf64d89d82421fa347c9
Toby2507 <oluwatobisalau2000@gmail.com>
2024-07-07 18:18:52 +0100

    Add new paths to the path helper function

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -2,12 +2,15 @@ const path = {
   home: () => '/',
   login: () => '/login',
   register: () => '/signup',
+  listProperty: () => '/listproperty',
+  partner: () => '/listproperty/partner',
   otp: (email: string) => `/otp/${email}`,
   forgotPassword: () => '#',
   profile: () => '#',
   userLicense: () => '#',
   offers: (id: string) => '#',
   stayDetail: (id: string) => `/stay/${id}`,
+  admin: () => '/admin',
 };
 
 export default path;
