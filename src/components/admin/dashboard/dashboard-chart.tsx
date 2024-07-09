5d4c0fb38a7ba583e0f55ebac806b7910363ea75
Toby2507 <oluwatobisalau2000@gmail.com>
2024-07-09 22:26:43 +0100

    Refactor the chart UI and add a loading spinner while fetching analytics

--- src/components/admin/dashboard/dashboard-chart.tsx
+++ src/components/admin/dashboard/dashboard-chart.tsx
@@ -1,44 +1,56 @@
 'use client';
 
-import { chartData } from '@/data';
+import { IReservationStats } from '@/types';
+import { ImSpinner2 } from 'react-icons/im';
 import { Area, AreaChart, CartesianGrid, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
-const DashboardChart = () => {
+interface IDashboardChart {
+  isLoading: boolean;
+  data: IReservationStats[];
+}
+
+const DashboardChart = ({ data, isLoading }: IDashboardChart) => {
   return (
     <ResponsiveContainer width="100%" minHeight={324} height="100%">
-      <AreaChart width={730} height={250} data={chartData} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
-        <defs>
-          <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#e42424" stopOpacity={0.8} />
-            <stop offset="95%" stopColor="#e42424" stopOpacity={0} />
-          </linearGradient>
-          <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#0075ff" stopOpacity={0.8} />
-            <stop offset="95%" stopColor="#0075ff" stopOpacity={0} />
-          </linearGradient>
-        </defs>
-        <XAxis dataKey="name" />
-        <YAxis />
-        <CartesianGrid strokeDasharray="3 3" />
-        <Tooltip />
-        <Legend iconType="square" iconSize={10} />
-        <Area
-          type="monotone"
-          dataKey="Reservations"
-          stroke="#0075ff"
-          strokeOpacity={0.15}
-          fillOpacity={0.55}
-          fill="url(#colorPv)"
-        />
-        <Area
-          type="monotone"
-          dataKey="Cancelled Reservations"
-          stroke="#e42424"
-          strokeOpacity={0.15}
-          fillOpacity={0.55}
-          fill="url(#colorUv)"
-        />
-      </AreaChart>
+      {isLoading ? (
+        <div className="grid place-items-center w-full h-72">
+          <ImSpinner2 className="text-6xl text-accentGray animate-spin" />
+        </div>
+      ) : (
+        <AreaChart width={730} height={250} data={data} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+          <defs>
+            <linearGradient id="cancelledReservations" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#e42424" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#e42424" stopOpacity={0} />
+            </linearGradient>
+            <linearGradient id="reservation" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#0075ff" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#0075ff" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="name" tickLine={false} />
+          <YAxis tickLine={false} />
+          <CartesianGrid stroke="#F2F2F299" />
+          <Tooltip />
+          <Legend iconType="square" iconSize={10} wrapperStyle={{ gap: 20 }} />
+          <Area
+            type="monotone"
+            dataKey="Reservations"
+            stroke="#0075ff"
+            strokeOpacity={0.15}
+            fillOpacity={0.55}
+            fill="url(#reservation)"
+          />
+          <Area
+            type="monotone"
+            dataKey="Cancelled Reservations"
+            stroke="#e42424"
+            strokeOpacity={0.15}
+            fillOpacity={0.55}
+            fill="url(#cancelledReservations)"
+          />
+        </AreaChart>
+      )}
     </ResponsiveContainer>
   );
 };
