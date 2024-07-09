9576fa9c71684723ad854b51a63fdb0c0a21edc0
Toby2507 <oluwatobisalau2000@gmail.com>
2024-07-09 22:39:01 +0100

    Decompose the user component making it reusable

--- src/components/misc/header-auth.tsx
+++ src/components/misc/header-auth.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-import { logout } from '@/server';
 import { IUserDetails } from '@/types';
 import { paths } from '@/utils';
-import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, User } from '@nextui-org/react';
+import { Button } from '@nextui-org/react';
 import Link from 'next/link';
+import HeaderUser from '../common/header-user';
 
 interface IHeaderAuth {
   user: IUserDetails | null;
@@ -14,31 +14,7 @@ const HeaderAuth = ({ user }: IHeaderAuth) => {
   return (
     <>
       {user ? (
-        <Dropdown placement="bottom-end">
-          <DropdownTrigger>
-            <User
-              as="button"
-              name={user.fullName}
-              description={<Link href={paths.profile()}>View Profile</Link>}
-              avatarProps={{ isBordered: true, src: user.imgUrl }}
-            />
-          </DropdownTrigger>
-          <DropdownMenu aria-label="Profile Actions" variant="flat">
-            <DropdownItem key="profile" className="h-14 gap-2">
-              <p className="font-semibold">Signed in as</p>
-              <p className="font-semibold">zoey@example.com</p>
-            </DropdownItem>
-            <DropdownItem key="settings">My Settings</DropdownItem>
-            <DropdownItem key="team_settings">Team Settings</DropdownItem>
-            <DropdownItem key="analytics">Analytics</DropdownItem>
-            <DropdownItem key="system">System</DropdownItem>
-            <DropdownItem key="configurations">Configurations</DropdownItem>
-            <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
-            <DropdownItem onClick={() => logout()} key="logout" color="danger">
-              Log Out
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
+        <HeaderUser user={user} />
       ) : (
         <>
           <Link href={paths.login()}>
