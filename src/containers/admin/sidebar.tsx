644fb3cc235b221ec86eea16542bfa4e9253d893
Toby2507 <oluwatobisalau2000@gmail.com>
2024-07-09 22:44:47 +0100

    Refactor the sidebar container to fit the UI better and remove the logo

--- src/containers/admin/sidebar.tsx
+++ src/containers/admin/sidebar.tsx
@@ -5,12 +5,9 @@ import { RxCaretRight } from 'react-icons/rx';
 const AdminSidebar = () => {
   const [mainNav, footNav] = [adminNav.slice(0, -2), adminNav.slice(-2)];
   return (
-    <aside className="flex flex-col border-r border-default w-72 h-screen">
-      <nav className="flex-1 flex flex-col gap-14 px-4 py-10">
-        <Link href="admin">
-          <h1 className="text-3xl font-bold text-primary cursor-pointer">CityGuideX</h1>
-        </Link>
-        <ul className="flex flex-col">
+    <aside className="border-r border-default pt-20 w-64">
+      <nav className="flex flex-col gap-14 h-full">
+        <ul className="flex-1 flex flex-col px-4">
           {mainNav.map(({ Icon, href, title }, idx) => (
             <Link href={href} key={idx}>
               <li className="flex items-center gap-2 transition-all duration-500 ease-in-out py-3 hover:scale-105">
@@ -21,19 +18,19 @@ const AdminSidebar = () => {
             </Link>
           ))}
         </ul>
-      </nav>
-      <nav className="bg-bgGray px-4 py-10">
-        <ul className="flex flex-col gap-6">
-          {footNav.map(({ Icon, href, title }, idx) => (
-            <Link href={href} key={idx}>
-              <li className="flex items-center gap-2">
-                <Icon className="text-accentGray" />
-                <p className="flex-1 text-accentGray font-medium">{title}</p>
-                <RxCaretRight className="text-accentGray text-xl" />
-              </li>
-            </Link>
-          ))}
-        </ul>
+        <div className="bg-bgGray px-4 py-10">
+          <ul className="flex flex-col gap-6">
+            {footNav.map(({ Icon, href, title }, idx) => (
+              <Link href={href} key={idx}>
+                <li className="flex items-center gap-2">
+                  <Icon className="text-accentGray" />
+                  <p className="flex-1 text-accentGray font-medium">{title}</p>
+                  <RxCaretRight className="text-accentGray text-xl" />
+                </li>
+              </Link>
+            ))}
+          </ul>
+        </div>
       </nav>
     </aside>
   );
