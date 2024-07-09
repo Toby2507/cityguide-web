a42fbe9f12b79e6d67c8a1570d02fd13601a6b90
Toby2507 <oluwatobisalau2000@gmail.com>
2024-07-09 22:21:59 +0100

    Move the admin routes from a group route and refactor the UI

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,24 @@
+import { AdminHeader } from '@/components';
+import { AdminSidebar } from '@/containers';
+import { getUser } from '@/server';
+import { redirect } from 'next/navigation';
+
+interface ILayout {
+  children: React.ReactNode;
+}
+
+const Layout = async ({ children }: Readonly<ILayout>) => {
+  const cookie = await getUser();
+  if (!cookie) redirect('/');
+  return (
+    <div className="flex flex-col bg-white max-h-screen">
+      <AdminHeader />
+      <div className="flex">
+        <AdminSidebar />
+        <main className="px-8 pt-24 pb-4 h-screen overflow-y-auto w-full adminmain">{children}</main>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
