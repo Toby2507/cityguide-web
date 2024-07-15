f8afd0ddeef1ac46e9d0c5f14378b0bd3789effe
Toby2507 <oluwatobisalau2000@gmail.com>
2024-07-15 19:53:16 +0100

    Add the create property interface file

---
+++ src/types/create.property.interfaces.ts
@@ -0,0 +1,31 @@
+import { MaxDays, Rating, StayType } from './enums';
+import { IAccommodation, IAddress } from './model.interfaces';
+
+export interface ICreateStay {
+  type: StayType;
+  name: string;
+  summary: string;
+  // extraInfo?: {
+  //   host?: {
+  //     name: string;
+  //     info: string;
+  //   };
+  //   property?: string;
+  //   neighborhood?: string;
+  // };
+  // address: IAddress;
+  avatar: string;
+  images?: string[];
+  amenities: string[];
+  hotelRating: Rating;
+  rules: {
+    checkIn: string;
+    checkOut: string;
+    smoking: boolean;
+    pets: boolean;
+    parties: boolean;
+  };
+  // accommodation: IAccommodation[];
+  maxDays: MaxDays;
+  language: string[];
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 export * from './admin.interfaces';
 export * from './auth.interfaces';
 export * from './common.interfaces';
+export * from './create.property.interfaces';
 export * from './enums';
 export * from './model.interfaces';
 export * from './store.interfaces';
