822bb8a91f94911a6cf87cba9e44791ffb80a9bb
Toby2507 <oluwatobisalau2000@gmail.com>
2024-07-16 19:13:37 +0100

    Add the conditionally rendered component for the step 3 tab

--- src/app/admin/stay/create/page.tsx
+++ src/app/admin/stay/create/page.tsx
@@ -1,16 +1,17 @@
 'use client';
 
-import { CreateStayStep1, CreateStayStep2 } from '@/components';
+import { CreateStayStep1, CreateStayStep2, CreateStayStep3, CreateStayStep4 } from '@/components';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 const CreateStayPage = () => {
   const [step, setStep] = useState<number>(1);
-  const { control, watch, reset, trigger, handleSubmit } = useForm();
+  const { control, watch, reset, trigger, setFocus, handleSubmit } = useForm();
   return (
     <form>
       {step === 1 ? <CreateStayStep1 control={control} setStep={setStep} /> : null}
       {step === 2 ? <CreateStayStep2 control={control} setStep={setStep} /> : null}
+      {step === 3 ? <CreateStayStep3 control={control} trigger={trigger} watch={watch} setStep={setStep} /> : null}
     </form>
   );
 };
