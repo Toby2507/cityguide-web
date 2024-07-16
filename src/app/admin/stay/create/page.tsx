35a43f1cbf6ff54b453f2388a2ae5b8f0504cdbe
Toby2507 <oluwatobisalau2000@gmail.com>
2024-07-16 17:19:48 +0100

    Add the components for the 1st and 2nd tabs of the form

--- src/app/admin/stay/create/page.tsx
+++ src/app/admin/stay/create/page.tsx
@@ -1,5 +1,18 @@
+'use client';
+
+import { CreateStayStep1, CreateStayStep2 } from '@/components';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
 const CreateStayPage = () => {
-  return <div>CreateStayPage</div>;
+  const [step, setStep] = useState<number>(1);
+  const { control, watch, reset, trigger, handleSubmit } = useForm();
+  return (
+    <form>
+      {step === 1 ? <CreateStayStep1 control={control} setStep={setStep} /> : null}
+      {step === 2 ? <CreateStayStep2 control={control} setStep={setStep} /> : null}
+    </form>
+  );
 };
 
 export default CreateStayPage;
