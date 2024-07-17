6cb2305e38a947314fe6688351a18c25f24bd3ba
Toby2507 <oluwatobisalau2000@gmail.com>
2024-07-17 19:40:58 +0100

    Add the isLoading prop

--- src/components/admin/stay/create-stay-btns.tsx
+++ src/components/admin/stay/create-stay-btns.tsx
@@ -1,17 +1,25 @@
 import { Button } from '@nextui-org/react';
 
 interface ICreateStayButtons {
+  isLoading: boolean;
   next: () => void;
   previous: () => void;
 }
 
-const CreateStayButtons = ({ next, previous }: ICreateStayButtons) => {
+const CreateStayButtons = ({ isLoading, next, previous }: ICreateStayButtons) => {
   return (
     <div className="flex items-center justify-center gap-4">
       <Button className="text-sm font-semibold px-14" color="primary" radius="full" variant="flat" onClick={previous}>
         Previous
       </Button>
-      <Button className="text-sm font-semibold px-14" color="primary" radius="full" variant="solid" onClick={next}>
+      <Button
+        className="text-sm font-semibold px-14"
+        color="primary"
+        radius="full"
+        variant="solid"
+        onClick={next}
+        isLoading={isLoading}
+      >
         Next
       </Button>
     </div>
