43ee3c0d6a14ed33f533195acbcf3c3441e888b6
Toby2507 <oluwatobisalau2000@gmail.com>
2024-07-18 22:58:47 +0100

    Add an optional property to render a custom name for the next button

--- src/components/admin/stay/create-stay-btns.tsx
+++ src/components/admin/stay/create-stay-btns.tsx
@@ -2,11 +2,12 @@ import { Button } from '@nextui-org/react';
 
 interface ICreateStayButtons {
   isLoading: boolean;
+  nextText?: string;
   next: () => void;
   previous: () => void;
 }
 
-const CreateStayButtons = ({ isLoading, next, previous }: ICreateStayButtons) => {
+const CreateStayButtons = ({ isLoading, nextText, next, previous }: ICreateStayButtons) => {
   return (
     <div className="flex items-center justify-center gap-4">
       <Button className="text-sm font-semibold px-14" color="primary" radius="full" variant="flat" onClick={previous}>
@@ -20,7 +21,7 @@ const CreateStayButtons = ({ isLoading, next, previous }: ICreateStayButtons) =>
         onClick={next}
         isLoading={isLoading}
       >
-        Next
+        {nextText || 'Next'}
       </Button>
     </div>
   );
