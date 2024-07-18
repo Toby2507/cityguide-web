ab8dc4ea0fc02447d2a7463319c3a7895a37e2dd
Toby2507 <oluwatobisalau2000@gmail.com>
2024-07-18 23:16:37 +0100

    Refactor the component, add an optional create stay btns, add the handleUpload function

--- src/components/admin/stay/create-stay-6.tsx
+++ src/components/admin/stay/create-stay-6.tsx
@@ -1,31 +1,59 @@
 'use client';
 
+import { uploadImages } from '@/server';
 import { formatFileSize } from '@/utils';
-import { Button, Card, Image } from '@nextui-org/react';
-import { useCallback, useState } from 'react';
+import { Button, Image } from '@nextui-org/react';
+import { Dispatch, SetStateAction, useCallback, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
+import { Control, FieldValues, useController } from 'react-hook-form';
 import { IoClose, IoCloudUploadOutline } from 'react-icons/io5';
+import CreateStayButtons from './create-stay-btns';
 
-interface CustomFile {
+interface ICreateStay {
+  control: Control<FieldValues>;
+  setStep: Dispatch<SetStateAction<number>>;
+}
+interface ICustomFile {
   id: string;
   file: File;
 }
 
-const CreateStayStep6 = () => {
-  const [avatar, setAvatar] = useState<CustomFile>();
-  const [images, setImages] = useState<CustomFile[]>([]);
+const CreateStayStep6 = ({ control, setStep }: ICreateStay) => {
+  const [avatar, setAvatar] = useState<string>('');
+  const [images, setImages] = useState<ICustomFile[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const onDrop = useCallback(
     (files: File[]) => {
       let tFiles = files.map((f) => ({ id: `${f.name}-${f.size}-${f.type}`, file: f }));
       tFiles = tFiles.filter((file) => !images.some((i) => i.id === file.id));
-      if (!avatar) setAvatar(tFiles[0]);
+      if (!avatar) setAvatar(tFiles[0].id);
       setImages((prev) => [...prev, ...tFiles]);
     },
     [avatar, images]
   );
+  const {
+    field: { onChange: changeAvatar, value },
+  } = useController({ control, name: 'avatar' });
+  const {
+    field: { onChange: changeImages },
+  } = useController({ control, name: 'images' });
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   const removeFile = (id: string) => setImages((prev) => prev.filter((file) => file.id !== id));
+  const handleUpload = async () => {
+    setIsLoading(true);
+    const avatarData = new FormData();
+    const imagesData = new FormData();
+    images.forEach((image) => {
+      if (image.id === avatar) avatarData.append('images', image.file);
+      else imagesData.append('images', image.file);
+    });
+    const imgUrls = await Promise.all([uploadImages(avatarData), uploadImages(imagesData)]);
+    changeAvatar(imgUrls[0][0]);
+    changeImages(imgUrls[1]);
+    setIsLoading(false);
+    setStep(7);
+  };
   return (
     <div className="flex flex-col justify-center gap-4">
       <div className="flex flex-col gap-2">
@@ -48,15 +76,15 @@ const CreateStayStep6 = () => {
           <span className="text-xs text-accentGray font-light">Maximum size: 10MB</span>
         </div>
         <p className="text-xs text-center text-accentGray font-light pt-4">
-          Click on any selected image to set it as your display image
+          Click on any of the selected images to set it as your display image
         </p>
         <div className="grid grid-cols-3 gap-2 items-center">
           {images.map(({ file, id }) => (
             <div
               key={id}
-              onClick={() => setAvatar({ file, id })}
+              onClick={() => setAvatar(id)}
               className={`flex items-center gap-2 p-2 rounded-xl cursor-pointer ${
-                id === avatar?.id ? 'bg-secondary/60' : 'bg-bgAccentBlue'
+                id === avatar ? 'bg-secondary/60' : 'bg-bgAccentBlue'
               }`}
             >
               <figure className="h-14 w-14">
@@ -73,6 +101,9 @@ const CreateStayStep6 = () => {
           ))}
         </div>
       </div>
+      {images.length || value ? (
+        <CreateStayButtons isLoading={isLoading} nextText="Upload" previous={() => setStep(5)} next={handleUpload} />
+      ) : null}
     </div>
   );
 };
