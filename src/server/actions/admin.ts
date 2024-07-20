07dfe27ff353338a9f8dcdd1083d7c5060ffa9ea
Toby2507 <oluwatobisalau2000@gmail.com>
2024-07-20 04:15:27 +0100

    Refactor the uploadImages action, add a true value for the ismutlipart parameter of the fetch with reauth function

--- src/server/actions/admin.ts
+++ src/server/actions/admin.ts
@@ -3,7 +3,7 @@
 import { fetchWithReAuth } from '@/utils';
 
 export const uploadImages = async (body: FormData) => {
-  const res = await fetchWithReAuth('account/upload', { method: 'POST', body });
+  const res = await fetchWithReAuth('account/upload', { method: 'POST', body }, true);
   const images = await res.json();
   return images.imgUrls;
 };
