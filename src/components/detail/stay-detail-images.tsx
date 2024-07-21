614d2532305426403909a6e8be3e0afed2857a6b
Toby2507 <oluwatobisalau2000@gmail.com>
2024-07-21 07:43:27 +0100

    Refactor the component to alter the arrangement of the images depending on the length of the images array

--- src/components/detail/stay-detail-images.tsx
+++ src/components/detail/stay-detail-images.tsx
@@ -1,31 +1,49 @@
-import avatar from '@images/detail-avatar.png';
-import img1 from '@images/detail-image-1.png';
-import img2 from '@images/detail-image-2.png';
-import img3 from '@images/detail-image-3.png';
-import img4 from '@images/detail-image-4.png';
-import img5 from '@images/detail-image-5.png';
 import { Image } from '@nextui-org/react';
 
-const images = [img1, img2, img3, img4, img5];
+interface IProps {
+  avatar: string;
+  images?: string[];
+}
 
-const StayDetailImages = () => {
+const StayDetailImages = ({ images, avatar }: IProps) => {
+  let avatarDim = '',
+    imagesDim = '',
+    initImgDim = '';
+  if (!images?.length) avatarDim = 'col-span-6 row-span-6';
+  if ((images?.length || 0) >= 1) {
+    avatarDim = 'col-span-4 row-span-6';
+    initImgDim = 'col-span-2 row-span-6';
+  }
+  if ((images?.length || 0) >= 2) {
+    initImgDim = 'col-span-2 row-span-3 h-72';
+    imagesDim = 'col-span-2 row-span-3 h-72';
+  }
+  if ((images?.length || 0) >= 3) {
+    avatarDim = 'col-span-4 row-span-4 h-[24.5rem]';
+    initImgDim = 'col-span-2 row-span-4';
+    imagesDim = 'col-span-3 row-span-2 h-48';
+  }
+  if ((images?.length || 0) >= 4) imagesDim = 'col-span-2 row-span-2 h-48';
+  if ((images?.length || 0) >= 5) initImgDim = 'col-span-2 row-span-2 h-48';
   return (
-    <div className="col-span-3 grid grid-cols-subgrid grid-rows-3 auto-rows-[100px] gap-2">
-      <figure className="col-span-1 row-span-1 h-48 rounded-lg overflow-hidden">
+    <div className="col-span-6 grid grid-cols-subgrid grid-rows-6 auto-rows-[100px] gap-2">
+      {images?.length ? (
+        <figure className={`rounded-lg overflow-hidden ${initImgDim}`}>
+          <Image
+            src={images[0]}
+            width="full"
+            alt="stay"
+            height={224}
+            radius="none"
+            isZoomed
+            removeWrapper
+            className="object-cover h-full w-full"
+          />
+        </figure>
+      ) : null}
+      <figure className={`rounded-lg overflow-hidden ${avatarDim}`}>
         <Image
-          src={images[0].src}
-          width="full"
-          alt="stay"
-          height={224}
-          radius="none"
-          isZoomed
-          removeWrapper
-          className="object-cover h-full w-full"
-        />
-      </figure>
-      <figure className="col-span-2 row-span-2 h-[24.5rem] rounded-lg overflow-hidden">
-        <Image
-          src={avatar.src}
+          src={avatar}
           width="full"
           alt="avatar"
           height={224}
@@ -35,10 +53,10 @@ const StayDetailImages = () => {
           className="object-cover h-full w-full"
         />
       </figure>
-      {images.slice(1).map((img, i) => (
-        <figure key={i} className="col-span-1 row-span-1 h-48 rounded-lg overflow-hidden">
+      {images?.slice(1).map((img, i) => (
+        <figure key={i} className={`rounded-lg overflow-hidden ${imagesDim}`}>
           <Image
-            src={img.src}
+            src={img}
             width="full"
             alt="stay"
             height={100}
