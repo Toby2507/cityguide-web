3c0a933a203daeaf218dbf9de728a041e856679f
Toby2507 <oluwatobisalau2000@gmail.com>
2024-07-22 12:34:56 +0100

    Refactor the component making it dynamic via the stay prop

--- src/containers/detail/stay-detail-rules.tsx
+++ src/containers/detail/stay-detail-rules.tsx
@@ -2,12 +2,30 @@
 
 import { IStay } from '@/types';
 import { Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from '@nextui-org/react';
+import dayjs from 'dayjs';
+import { useMemo } from 'react';
+import { BsExclamationCircleFill } from 'react-icons/bs';
+import { IoCard, IoLogIn, IoLogOut } from 'react-icons/io5';
+import { MdOutlineFamilyRestroom, MdOutlinePets } from 'react-icons/md';
 
 interface IProps {
   stay: IStay;
 }
 
-const StayDetailRules = ({ stay }: IProps) => {
+const StayDetailRules = ({ stay: { rules, accommodation } }: IProps) => {
+  const [checkInFrom, checkInTo] = rules.checkIn.split('-');
+  const [checkOutFrom, checkOutTo] = rules.checkOut.split('-');
+
+  const checkIn = useMemo(
+    () => `${dayjs(`1-1-1 ${checkInFrom}`).format('hh:mm A')} - ${dayjs(`1-1-1 ${checkInTo}`).format('hh:mm A')}`,
+    [checkInFrom, checkInTo]
+  );
+  const checkOut = useMemo(
+    () => `${dayjs(`1-1-1 ${checkOutFrom}`).format('hh:mm A')} - ${dayjs(`1-1-1 ${checkOutTo}`).format('hh:mm A')}`,
+    [checkOutFrom, checkOutTo]
+  );
+  const children = useMemo(() => accommodation.some((a) => a.children), [accommodation]);
+  const infants = useMemo(() => accommodation.some((a) => a.infants), [accommodation]);
   return (
     <section className="flex flex-col gap-4 pb-10" id="rules">
       <header className="flex flex-col gap-2">
@@ -21,36 +39,53 @@ const StayDetailRules = ({ stay }: IProps) => {
         </TableHeader>
         <TableBody>
           <TableRow className="border-b border-default">
-            <TableCell className="py-4 font-medium">Check-in</TableCell>
-            <TableCell className="py-4 text-accentGray w-9/12">Specific time chosen on reservation</TableCell>
+            <TableCell className="flex items-center gap-2 py-4 font-medium">
+              <IoLogIn className="text-xl" />
+              Check-in
+            </TableCell>
+            <TableCell className="py-4 text-accentGray w-9/12">{checkIn}</TableCell>
           </TableRow>
           <TableRow className="border-b border-default">
-            <TableCell className="py-4 font-medium">Check-out</TableCell>
-            <TableCell className="py-4 text-accentGray w-9/12">From 7:00am - 10:00am</TableCell>
+            <TableCell className="flex items-center gap-2 py-4 font-medium">
+              <IoLogOut className="text-xl" />
+              Check-out
+            </TableCell>
+            <TableCell className="py-4 text-accentGray w-9/12">{checkOut}</TableCell>
           </TableRow>
           <TableRow className="border-b border-default">
-            <TableCell className="py-4 font-medium">Cancellation</TableCell>
+            <TableCell className="flex items-center gap-2 py-4 font-medium">
+              <BsExclamationCircleFill className="text-lg" />
+              Cancellation
+            </TableCell>
             <TableCell className="py-4 text-accentGray w-9/12">
               The terms of prepayment and cancellation differ depending on the kind of lodging. When choosing an option,
               make sure to check any possible conditions.
             </TableCell>
           </TableRow>
           <TableRow className="border-b border-default">
-            <TableCell className="py-4 font-medium">Age Requirement</TableCell>
-            <TableCell className="py-4 text-accentGray w-9/12">No age requirement</TableCell>
-          </TableRow>
-          <TableRow className="border-b border-default">
-            <TableCell className="py-4 font-medium">Pets</TableCell>
-            <TableCell className="py-4 text-accentGray w-9/12">Pets are not allowed</TableCell>
-          </TableRow>
-          <TableRow className="border-b border-default">
-            <TableCell className="py-4 font-medium">Groups</TableCell>
-            <TableCell className="py-4 text-accentGray w-9/12">
-              When booking more than 10 rooms, different policies and additional supplements may apply.
+            <TableCell className="flex items-center gap-2 py-4 font-medium">
+              <MdOutlineFamilyRestroom className="text-lg" />
+              Children & Infants
+            </TableCell>
+            <TableCell className="py-4 w-9/12">
+              <p className="text-accentGray pb-2">
+                Children of all ages are {children ? '' : 'not '}welcome at this property
+              </p>
+              <p className="text-accentGray">Infants are {infants ? '' : 'not '}welcome at this property</p>
             </TableCell>
           </TableRow>
+          <TableRow className="border-b border-default">
+            <TableCell className="flex items-center gap-2 py-4 font-medium">
+              <MdOutlinePets className="text-lg" />
+              Pets
+            </TableCell>
+            <TableCell className="py-4 text-accentGray w-9/12">Pets are not allowed</TableCell>
+          </TableRow>
           <TableRow>
-            <TableCell className="py-4 font-medium">Payment method</TableCell>
+            <TableCell className="flex items-center gap-2 py-4 font-medium">
+              <IoCard className="text-lg" />
+              Payment method
+            </TableCell>
             <TableCell className="py-4 text-accentGray w-9/12">Payments will be made on site.</TableCell>
           </TableRow>
         </TableBody>
