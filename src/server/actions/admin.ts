c66a604f7db6339497985446dfb21376c1b83d49
Toby2507 <oluwatobisalau2000@gmail.com>
2024-08-06 09:16:02 +0100

    Add the create stay action

--- src/server/actions/admin.ts
+++ src/server/actions/admin.ts
@@ -1,9 +1,16 @@
 'use server';
 
+import { ICreateStay } from '@/types';
 import { fetchWithReAuth } from '@/utils';
 
 export const uploadImages = async (body: FormData) => {
   const res = await fetchWithReAuth('account/upload', { method: 'POST', body }, true);
   const images = await res.json();
   return images.imgUrls;
+};
+
+export const createStay = async (body: ICreateStay) => {
+  const res = await fetchWithReAuth('stay/create', { method: 'POST', body: JSON.stringify(body) });
+  console.log(res);
+  const stay = await res.json();
 };
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,5 @@
 // Queries
-export { uploadImages } from './actions/admin';
+export { createStay, uploadImages } from './actions/admin';
 export { createEstablishment, createUser, loginUser, upgradeUser, verifyOtp } from './actions/auth';
 export { getReservationAnalytics } from './queries/admin';
 export { getUser, logout } from './queries/auth';
