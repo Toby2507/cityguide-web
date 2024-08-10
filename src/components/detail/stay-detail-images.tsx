88daf3f372659af6cb348de9a2d79420e69c5053
Toby2507 <oluwatobisalau2000@gmail.com>
2024-08-10 16:10:22 +0100

    Replace the dynamic image layout with a more static layout since images are guaranteed to be more than 6

--- src/components/detail/stay-detail-images.tsx
+++ src/components/detail/stay-detail-images.tsx
@@ -1,47 +1,26 @@
-import { Image } from '@nextui-org/react';
+import { Button, Image } from '@nextui-org/react';
 
 interface IProps {
   avatar: string;
-  images?: string[];
+  images: string[];
 }
 
 const StayDetailImages = ({ images, avatar }: IProps) => {
-  let avatarDim = '',
-    imagesDim = '',
-    initImgDim = '';
-  if (!images?.length) avatarDim = 'col-span-6 row-span-6';
-  if ((images?.length || 0) >= 1) {
-    avatarDim = 'col-span-4 row-span-6';
-    initImgDim = 'col-span-2 row-span-6';
-  }
-  if ((images?.length || 0) >= 2) {
-    initImgDim = 'col-span-2 row-span-3 h-72';
-    imagesDim = 'col-span-2 row-span-3 h-72';
-  }
-  if ((images?.length || 0) >= 3) {
-    avatarDim = 'col-span-4 row-span-4 h-[24.5rem]';
-    initImgDim = 'col-span-2 row-span-4';
-    imagesDim = 'col-span-3 row-span-2 h-48';
-  }
-  if ((images?.length || 0) >= 4) imagesDim = 'col-span-2 row-span-2 h-48';
-  if ((images?.length || 0) >= 5) initImgDim = 'col-span-2 row-span-2 h-48';
   return (
     <div className="col-span-6 grid grid-cols-subgrid grid-rows-6 auto-rows-[100px] gap-2">
-      {images?.length ? (
-        <figure className={`rounded-lg overflow-hidden ${initImgDim}`}>
-          <Image
-            src={images[0]}
-            width="full"
-            alt="stay"
-            height={224}
-            radius="none"
-            isZoomed
-            removeWrapper
-            className="object-cover h-full w-full"
-          />
-        </figure>
-      ) : null}
-      <figure className={`rounded-lg overflow-hidden ${avatarDim}`}>
+      <figure className="rounded-lg overflow-hidden col-span-2 row-span-2 h-48">
+        <Image
+          src={images[0]}
+          width="full"
+          alt="stay"
+          height={224}
+          radius="none"
+          isZoomed
+          removeWrapper
+          className="object-cover h-full w-full"
+        />
+      </figure>
+      <figure className="rounded-lg overflow-hidden col-span-4 row-span-4 h-[24.5rem]">
         <Image
           src={avatar}
           width="full"
@@ -53,8 +32,8 @@ const StayDetailImages = ({ images, avatar }: IProps) => {
           className="object-cover h-full w-full"
         />
       </figure>
-      {images?.slice(1).map((img, i) => (
-        <figure key={i} className={`rounded-lg overflow-hidden ${imagesDim}`}>
+      {images?.slice(1, 4).map((img, i) => (
+        <figure key={i} className="rounded-lg overflow-hidden col-span-2 row-span-2 h-48">
           <Image
             src={img}
             width="full"
@@ -67,6 +46,23 @@ const StayDetailImages = ({ images, avatar }: IProps) => {
           />
         </figure>
       ))}
+      <figure className="relative rounded-lg overflow-hidden col-span-2 row-span-2 h-48">
+        <Image
+          src={images[4]}
+          width="full"
+          alt="stay"
+          height={100}
+          radius="none"
+          isZoomed
+          removeWrapper
+          className="object-cover h-full w-full"
+        />
+        <div className="absolute grid place-items-center w-full h-full top-0 left-0 bg-black/40 z-10">
+          <Button className="text-base text-white" size="sm" variant="light">
+            + {images.length - 5} photos
+          </Button>
+        </div>
+      </figure>
     </div>
   );
 };
