a542d9daf69aef8e9ee605df2f2f69d854dcc2fc
Toby2507 <oluwatobisalau2000@gmail.com>
2024-08-11 11:28:43 +0100

    Remove the rendering of other properties from the admin properties container

--- src/containers/admin/home/properties.tsx
+++ src/containers/admin/home/properties.tsx
@@ -24,34 +24,6 @@ const AdminProperties = () => {
           ))}
         </div>
       </div>
-      <div className="flex flex-col">
-        <div className="flex items-center justify-between gap-20">
-          <h2 className="text-xl font-semibold">Restaurant Properties</h2>
-          <Button className="text-primary text-sm font-semibold" color="secondary" radius="full" size="sm">
-            <FiPlus />
-            Add New
-          </Button>
-        </div>
-        <div className="overflow-x-auto p-2 flex gap-4 min-w-0 max-w-full">
-          {stays.map((stay) => (
-            <StayCard key={stay._id} {...stay} />
-          ))}
-        </div>
-      </div>
-      <div className="flex flex-col">
-        <div className="flex items-center justify-between gap-20">
-          <h2 className="text-xl font-semibold">Night Life Properties</h2>
-          <Button className="text-primary text-sm font-semibold" color="secondary" radius="full" size="sm">
-            <FiPlus />
-            Add New
-          </Button>
-        </div>
-        <div className="overflow-x-auto p-2 flex gap-4 min-w-0 max-w-full">
-          {stays.map((stay) => (
-            <StayCard key={stay._id} {...stay} />
-          ))}
-        </div>
-      </div>
     </section>
   );
 };
