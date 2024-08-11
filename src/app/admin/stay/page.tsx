62ef236b6429e5ee4d08add98d6fe51da27b0bd7
Toby2507 <oluwatobisalau2000@gmail.com>
2024-08-11 11:23:20 +0100

    Update the stay list page for the admin route with dynamic stay data from the backend

--- src/app/admin/stay/page.tsx
+++ src/app/admin/stay/page.tsx
@@ -1,11 +1,32 @@
-import { AdminProperties } from '@/containers';
+import { StayCard } from '@/components';
+import { getPartnerStays } from '@/server';
+import { paths } from '@/utils';
+import { Button } from '@nextui-org/react';
+import Link from 'next/link';
+import { FiPlus } from 'react-icons/fi';
 
-const AdminStayDetailPage = () => {
+const AdminStayListPage = async () => {
+  const stays = await getPartnerStays();
   return (
-    <div className="flex flex-col gap-4">
-      <AdminProperties />
-    </div>
+    <section className="flex flex-col gap-2" id="properties">
+      <div className="flex flex-col">
+        <div className="flex items-center justify-between gap-20">
+          <h2 className="text-xl font-semibold">Stay Properties</h2>
+          <Link href={paths.createStay()}>
+            <Button className="text-primary text-sm font-semibold" color="secondary" radius="full" size="sm">
+              <FiPlus />
+              Add New
+            </Button>
+          </Link>
+        </div>
+        <div className="grid items-center px-2 py-6 gap-10 min-w-0 max-w-full">
+          {stays.map((stay) => (
+            <StayCard key={stay._id} {...stay} />
+          ))}
+        </div>
+      </div>
+    </section>
   );
 };
 
-export default AdminStayDetailPage;
+export default AdminStayListPage;
