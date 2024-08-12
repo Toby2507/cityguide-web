738513d8a56c4f11e0b6238c525ab142dd6b373e
Toby2507 <oluwatobisalau2000@gmail.com>
2024-08-12 03:37:45 +0100

    Update the prop and their use in the place card component to render trending destinations

--- src/components/home/place-card.tsx
+++ src/components/home/place-card.tsx
@@ -1,14 +1,14 @@
+import { IStay } from '@/types';
 import { paths } from '@/utils';
-import { IOffers } from '@/types';
 import { Card, CardBody, CardFooter, Image } from '@nextui-org/react';
 import Link from 'next/link';
 
-const PlaceCard = ({ id, name, description, imgUrl }: IOffers) => {
+const PlaceCard = ({ _id, name, summary, avatar }: IStay) => {
   return (
     <Card shadow="none" className="grid grid-rows-2">
       <CardBody className="p-0">
         <Image
-          src={imgUrl.src}
+          src={avatar}
           width="full"
           alt={name}
           height={224}
@@ -20,9 +20,9 @@ const PlaceCard = ({ id, name, description, imgUrl }: IOffers) => {
       <CardFooter className="flex flex-col items-start justify-between border border-accentGray2/50 px-6 pb-6">
         <div className="flex flex-col gap-1">
           <h3 className="text-xl font-bold">{name}</h3>
-          <p className="text-xs font-normal">{description}</p>
+          <p className="text-xs font-normal">{summary.split('\n')[0]}</p>
         </div>
-        <Link href={paths.stayDetail(id)}>
+        <Link href={paths.stayDetail(_id)}>
           <p className="text-xs text-primary font-semibold hover:underline">See more {' >'}</p>
         </Link>
       </CardFooter>
