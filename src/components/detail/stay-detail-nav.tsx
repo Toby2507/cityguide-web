7f221a804ed19b607949314dcda26168cdb393a9
Toby2507 <oluwatobisalau2000@gmail.com>
2024-08-16 16:27:46 +0100

    Add move the stay search bar from the stay detail page

--- src/components/detail/stay-detail-nav.tsx
+++ src/components/detail/stay-detail-nav.tsx
@@ -1,25 +1,33 @@
+'use client';
+
+import { StaySearchBar } from '@/containers';
 import { Button, ButtonGroup } from '@nextui-org/react';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 const StayDetailNav = () => {
+  const { push } = useRouter();
   return (
-    <ButtonGroup as="nav" className="w-full">
-      <Button className="flex-1 border-b border-primary" variant="light" radius="none">
-        <Link href="#overview">Overview</Link>
-      </Button>
-      <Button className="flex-1 border-b border-default" variant="light" radius="none">
-        <Link href="#amenities">Amenities</Link>
-      </Button>
-      <Button className="flex-1 border-b border-default" variant="light" radius="none">
-        <Link href="#availability">Apartment Info & Price</Link>
-      </Button>
-      <Button className="flex-1 border-b border-default" variant="light" radius="none">
-        <Link href="#rules">House Rules</Link>
-      </Button>
-      <Button className="flex-1 border-b border-default" variant="light" radius="none">
-        <Link href="#reviews">Review</Link>
-      </Button>
-    </ButtonGroup>
+    <>
+      <StaySearchBar extraClass="-mt-7" search={() => push('#availability')} />
+      <ButtonGroup as="nav" className="w-full">
+        <Button className="flex-1 border-b border-primary" variant="light" radius="none">
+          <Link href="#overview">Overview</Link>
+        </Button>
+        <Button className="flex-1 border-b border-default" variant="light" radius="none">
+          <Link href="#amenities">Amenities</Link>
+        </Button>
+        <Button className="flex-1 border-b border-default" variant="light" radius="none">
+          <Link href="#availability">Apartment Info & Price</Link>
+        </Button>
+        <Button className="flex-1 border-b border-default" variant="light" radius="none">
+          <Link href="#rules">House Rules</Link>
+        </Button>
+        <Button className="flex-1 border-b border-default" variant="light" radius="none">
+          <Link href="#reviews">Review</Link>
+        </Button>
+      </ButtonGroup>
+    </>
   );
 };
 
