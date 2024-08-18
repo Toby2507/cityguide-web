6254a1643c294793984e7e6189c306cd2d501696
Toby2507 <oluwatobisalau2000@gmail.com>
2024-08-18 07:50:29 +0100

    filter out the restaurant and night life navs from the sidebar if entitytype is user

--- src/containers/admin/sidebar.tsx
+++ src/containers/admin/sidebar.tsx
@@ -1,21 +1,27 @@
 'use client';
 
 import { adminNav } from '@/data';
+import { EntityType } from '@/types';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { useMemo } from 'react';
 import { RxCaretRight } from 'react-icons/rx';
 
+interface Props {
+  type: EntityType;
+}
 const [mainNav, footNav] = [adminNav.slice(0, -2), adminNav.slice(-2)];
 
-const AdminSidebar = () => {
+const AdminSidebar = ({ type }: Props) => {
   const route = usePathname();
   const paths = useMemo(() => route.split('/').filter((crumb) => crumb), [route]);
+  const topNav =
+    type === EntityType.USER ? mainNav.filter((nav) => !['Restaurants', 'Night Life'].includes(nav.title)) : mainNav;
   return (
     <aside className="border-r border-default pt-20 w-64">
       <nav className="flex flex-col gap-14 h-full">
         <ul className="flex-1 flex flex-col px-4">
-          {mainNav.map(({ Icon, href, title }, idx) => {
+          {topNav.map(({ Icon, href, title }, idx) => {
             const isActive = `/${paths.slice(0, 2).join('/')}` === href;
             return (
               <Link href={href} key={idx}>
