bc833f5a856911e39b8781c17ce7774d077a78ab
Toby2507 <oluwatobisalau2000@gmail.com>
2024-08-18 21:53:58 +0100

    Add the create restaurant for m's 6th step tab component

--- src/app/admin/restaurant/create/page.tsx
+++ src/app/admin/restaurant/create/page.tsx
@@ -6,6 +6,7 @@ import {
   CreateRestaurantStep2,
   CreateRestaurantStep3,
   CreateRestaurantStep4,
+  CreateRestaurantStep6,
 } from '@/components';
 import { ICreateRestaurant } from '@/types';
 import { Pagination } from '@nextui-org/react';
@@ -27,6 +28,7 @@ const CreateRestaurantPage = () => {
       {step === 3 ? <CreateRestaurantStep3 setStep={setStep} /> : null}
       {step === 4 ? <CreateRestaurantStep4 setStep={setStep} /> : null}
       {step === 5 ? <CreatePropertyImageUpload name="Restaurant" nextStep={6} setStep={setStep} /> : null}
+      {step === 6 ? <CreateRestaurantStep6 setStep={setStep} /> : null}
       <div className="absolute bottom-10 flex flex-col gap-2">
         <p className="text-xs">Go to step</p>
         <Pagination total={topStep} page={step} onChange={(val) => setStep(val)} />
