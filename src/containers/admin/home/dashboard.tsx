f18d72c58d1cb512cd64f48fe5fae4ff43cc40ac
Toby2507 <oluwatobisalau2000@gmail.com>
2024-08-18 07:49:12 +0100

    Update the propname fill-opacity in of svg's to fillOpacity

--- src/containers/admin/home/dashboard.tsx
+++ src/containers/admin/home/dashboard.tsx
@@ -86,7 +86,7 @@ const AdminDashboard = () => {
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
               <path
                 fill="#0075FF"
-                fill-opacity="1"
+                fillOpacity="1"
                 d="M0,192L120,170.7C240,149,480,107,720,106.7C960,107,1200,149,1320,170.7L1440,192L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"
               ></path>
             </svg>
