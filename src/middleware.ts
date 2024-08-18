fab89123b3032e41535b718a60663680cea67ca1
Toby2507 <oluwatobisalau2000@gmail.com>
2024-08-18 07:56:37 +0100

    Add a middleware file that handles authorization

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import { EntityType } from '@/types';
+import { paths } from '@/utils';
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const userType = request.cookies.get('city-guide-type')?.value;
+  const isPartner = request.cookies.get('city-guide-partner')?.value;
+  const isAdminRoute = request.nextUrl.pathname.startsWith('/admin');
+  const isNextRoute = request.nextUrl.pathname.startsWith('/_next');
+  if (userType === EntityType.ESTABLISHMENT && !isAdminRoute && !isNextRoute)
+    return NextResponse.redirect(new URL(paths.admin(), request.url));
+  if (userType === EntityType.USER && !isPartner && isAdminRoute)
+    return NextResponse.redirect(new URL(paths.home(), request.url));
+  return NextResponse.next();
+}
