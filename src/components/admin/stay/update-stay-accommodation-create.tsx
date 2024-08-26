7f3f4de53be257549039fafea9f9a94bd4195a25
Toby2507 <oluwatobisalau2000@gmail.com>
2024-08-26 21:02:58 +0100

    Add the add accommodation modal for the update accommodations modal

---
+++ src/components/admin/stay/update-stay-accommodation-create.tsx
@@ -0,0 +1,101 @@
+'use client';
+
+import { addAccommodationSchema } from '@/schemas';
+import { addAccommodation } from '@/server';
+import { IAccommodationForm } from '@/types';
+import { Button, Pagination } from '@nextui-org/react';
+import { useState } from 'react';
+import { FormProvider, SubmitHandler, useFieldArray, useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import { IoAdd, IoRemove } from 'react-icons/io5';
+import AccommodationInputs from './accommodation-inputs';
+
+interface Props {
+  stayId: string;
+  onClose: () => void;
+}
+
+const UpdateStayAccommodationCreate = ({ stayId, onClose }: Props) => {
+  const method = useForm<IAccommodationForm>();
+  const { control, handleSubmit, reset, trigger } = method;
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [accIdx, setAccIdx] = useState<number>(0);
+  const [total, setTotal] = useState(1);
+  const { remove } = useFieldArray({ control, name: 'accommodation' });
+
+  const addAcc = () => {
+    setTotal(total + 1);
+    setAccIdx(accIdx + 1);
+  };
+  const removeAcc = () => {
+    remove(accIdx);
+    setTotal(total - 1);
+    setAccIdx(accIdx - 1);
+  };
+  const onSubmit: SubmitHandler<IAccommodationForm> = async (data) => {
+    setIsLoading(true);
+    try {
+      const [isValidT, isValidS] = await Promise.all([
+        trigger('accommodation'),
+        addAccommodationSchema.shape.body.safeParseAsync(data.accommodation),
+      ]);
+      if (!isValidT) return toast.error('Please fill out the required fields');
+      if (!isValidS.success) return toast.error('Please fill out the required fields in the other accommodations');
+      await addAccommodation(data.accommodation, stayId);
+      onClose();
+      reset();
+      toast.success('Accommodation(s) added successfully!');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <FormProvider {...method}>
+      <div className="flex flex-col gap-2">
+        <AccommodationInputs key={accIdx} idx={accIdx} />
+        <div className="flex items-center justify-between gap-10">
+          <Pagination total={total} page={accIdx + 1} onChange={(val) => setAccIdx(val - 1)} />
+          <div className="flex items-center gap-2">
+            <Button
+              className="text-xs font-medium pr-4"
+              color="primary"
+              radius="full"
+              size="sm"
+              variant="flat"
+              onPress={addAcc}
+              isLoading={isLoading}
+              startContent={<IoAdd className="text-lg" />}
+            >
+              New
+            </Button>
+            {accIdx ? (
+              <Button
+                className="text-xs font-medium pr-4"
+                color="danger"
+                radius="full"
+                size="sm"
+                variant="flat"
+                onPress={removeAcc}
+                isLoading={isLoading}
+                startContent={<IoRemove className="text-lg" />}
+              >
+                Remove
+              </Button>
+            ) : null}
+          </div>
+        </div>
+        <Button
+          className="text-sm font-semibold px-14 py-6 my-6"
+          color="primary"
+          radius="full"
+          onPress={() => handleSubmit(onSubmit)()}
+          isLoading={isLoading}
+        >
+          Add Accommodations
+        </Button>
+      </div>
+    </FormProvider>
+  );
+};
+
+export default UpdateStayAccommodationCreate;
--- src/components/admin/stay/update-stay-accommodation.tsx
+++ src/components/admin/stay/update-stay-accommodation.tsx
@@ -4,6 +4,7 @@ import { IStay } from '@/types';
 import { Button } from '@nextui-org/react';
 import { useState } from 'react';
 import { IoAdd } from 'react-icons/io5';
+import UpdateStayAccommodationCreate from './update-stay-accommodation-create';
 import UpdateStayAccommodationList from './update-stay-accommodation-list';
 import UpdateStayAccommodationUpdate from './update-stay-accommodation-update';
 
@@ -26,20 +27,24 @@ const UpdateStayAccommodation = ({ stay, onClose }: Props) => {
     <div className="flex flex-col gap-6 p-2">
       <header className="flex items-center justify-between gap-4 border-b py-2">
         <h3 className="text-2xl text-center font-semibold tracking-wide">Update Stay Accommodations</h3>
-        <Button
-          color="primary"
-          className="font-semibold"
-          radius="sm"
-          startContent={<IoAdd className="text-lg" />}
-          variant="flat"
-        >
-          Add New Accommodation
-        </Button>
+        {modalState === 'list' ? (
+          <Button
+            color="primary"
+            className="font-semibold"
+            radius="sm"
+            startContent={<IoAdd className="text-lg" />}
+            variant="flat"
+            onPress={() => setModalState('create')}
+          >
+            Add New Accommodation
+          </Button>
+        ) : null}
       </header>
       {modalState === 'list' ? <UpdateStayAccommodationList onUpdate={updateAccommodation} stay={stay} /> : null}
       {modalState === 'update' ? (
         <UpdateStayAccommodationUpdate accommodation={activeStay!} onClose={onClose} stayId={stay._id} />
       ) : null}
+      {modalState === 'create' ? <UpdateStayAccommodationCreate onClose={onClose} stayId={stay._id} /> : null}
     </div>
   );
 };
