79332159d4e23bf1357a02fa4cc396c20627ccef
Toby2507 <oluwatobisalau2000@gmail.com>
2024-08-26 19:59:20 +0100

    Update the stay accommodation update modal

--- src/components/admin/stay/update-stay-accommodation.tsx
+++ src/components/admin/stay/update-stay-accommodation.tsx
@@ -5,6 +5,7 @@ import { Button } from '@nextui-org/react';
 import { useState } from 'react';
 import { IoAdd } from 'react-icons/io5';
 import UpdateStayAccommodationList from './update-stay-accommodation-list';
+import UpdateStayAccommodationUpdate from './update-stay-accommodation-update';
 
 interface Props {
   stay: IStay;
@@ -15,6 +16,7 @@ type modalState = 'list' | 'update' | 'create';
 const UpdateStayAccommodation = ({ stay, onClose }: Props) => {
   const [modalState, setModalState] = useState<modalState>('list');
   const [accId, setAccId] = useState<string>('');
+  const activeStay = stay.accommodation.find((a) => a.id === accId);
 
   const updateAccommodation = (id: string) => {
     setModalState('update');
@@ -35,6 +37,9 @@ const UpdateStayAccommodation = ({ stay, onClose }: Props) => {
         </Button>
       </header>
       {modalState === 'list' ? <UpdateStayAccommodationList onUpdate={updateAccommodation} stay={stay} /> : null}
+      {modalState === 'update' ? (
+        <UpdateStayAccommodationUpdate accommodation={activeStay!} onClose={onClose} stayId={stay._id} />
+      ) : null}
     </div>
   );
 };
