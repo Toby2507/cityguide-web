46542726c2c6283b561ed147a7f89584ea0712d4
Toby2507 <oluwatobisalau2000@gmail.com>
2024-08-28 09:55:11 +0100

    Add the restaurant detail page for the admin route with the options to update the restaurant

---
+++ src/app/admin/restaurant/[restaurantId]/page.tsx
@@ -0,0 +1,16 @@
+import { AdminRestaurantDetail } from '@/containers';
+import { getRestaurantById } from '@/server';
+
+interface Props {
+  params: {
+    restaurantId: string;
+  };
+}
+
+const AdminRestaurantDetailPage = async ({ params: { restaurantId } }: Props) => {
+  const restaurant = await getRestaurantById(restaurantId);
+  if (!restaurant) return null;
+  return <AdminRestaurantDetail restaurant={restaurant} />;
+};
+
+export default AdminRestaurantDetailPage;
