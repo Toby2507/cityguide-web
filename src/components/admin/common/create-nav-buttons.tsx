b39f2f14fffeb6094e66eba9209bab29fcbde735
Toby2507 <oluwatobisalau2000@gmail.com>
2024-08-28 09:40:04 +0100

    Update the create nav button to take an optional is disabled prop

--- src/components/admin/common/create-nav-buttons.tsx
+++ src/components/admin/common/create-nav-buttons.tsx
@@ -2,15 +2,23 @@ import { Button } from '@nextui-org/react';
 
 interface Props {
   isLoading: boolean;
+  isDisabled?: boolean;
   nextText?: string;
   next: () => void;
   previous: () => void;
 }
 
-const CreateNavButtons = ({ isLoading, nextText, next, previous }: Props) => {
+const CreateNavButtons = ({ isDisabled, isLoading, nextText, next, previous }: Props) => {
   return (
     <div className="flex items-center justify-center gap-4">
-      <Button className="text-sm font-semibold px-14" color="primary" radius="full" variant="flat" onPress={previous}>
+      <Button
+        className="text-sm font-semibold px-14"
+        color="primary"
+        isDisabled={isDisabled}
+        radius="full"
+        variant="flat"
+        onPress={previous}
+      >
         Previous
       </Button>
       <Button
@@ -19,6 +27,7 @@ const CreateNavButtons = ({ isLoading, nextText, next, previous }: Props) => {
         radius="full"
         variant="solid"
         onPress={next}
+        isDisabled={isDisabled}
         isLoading={isLoading}
       >
         {nextText || 'Next'}
