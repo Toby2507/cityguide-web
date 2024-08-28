c8d2b9e239e9e46adee5223863ad3932cfdeea93
Toby2507 <oluwatobisalau2000@gmail.com>
2024-08-28 17:35:47 +0100

    Update the menu detail section of the restaurant detail page to render the update menu items button on the admin route

--- src/containers/detail/restaurant/menu.tsx
+++ src/containers/detail/restaurant/menu.tsx
@@ -1,18 +1,27 @@
 import { MenuItemCard } from '@/components';
 import { IMenu } from '@/types';
+import { Button } from '@nextui-org/react';
 
 interface Props {
   menu: IMenu[];
+  onUpdate?: () => void;
 }
 
-const RestaurantDetailMenu = ({ menu }: Props) => {
+const RestaurantDetailMenu = ({ menu, onUpdate }: Props) => {
   return (
     <section className="flex flex-col gap-4 pb-10" id="availability">
-      <header className="flex flex-col gap-2">
-        <h1 className="text-4xl font-bold capitalize">Menu</h1>
-        <p className="flex items-center gap-1 text-sm text-accentGray font-medium">
-          What&apos;s on the menu? Check out our offerings
-        </p>
+      <header className="flex items-center justify-between gap-10">
+        <div className="flex flex-col gap-2">
+          <h1 className="text-4xl font-bold capitalize">Menu</h1>
+          <p className="flex items-center gap-1 text-sm text-accentGray font-medium">
+            What&apos;s on the menu? Check out our offerings
+          </p>
+        </div>
+        {onUpdate ? (
+          <Button color="primary" className="px-10 font-semibold" onPress={onUpdate} radius="sm">
+            Update Menu
+          </Button>
+        ) : null}
       </header>
       <div className="grid grid-cols-2 gap-4">
         {menu.map((menu) => (
