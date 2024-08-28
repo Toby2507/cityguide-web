ec94799a84fe5ea6a6ef54e43b7be7cee5e4d08d
Toby2507 <oluwatobisalau2000@gmail.com>
2024-08-28 13:58:57 +0100

    Add the update restaurant info modal implementation

---
+++ src/components/admin/restaurant/contact-reservation-info.tsx
@@ -0,0 +1,285 @@
+'use client';
+
+import { updateRestaurantSchema } from '@/schemas';
+import { ICreateRestaurant, ISocialLink } from '@/types';
+import { onEnter } from '@/utils';
+import { Button, Input, Select, SelectItem } from '@nextui-org/react';
+import { useState } from 'react';
+import { Controller, useController, useFormContext } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import { IoAdd } from 'react-icons/io5';
+import StringArrayInput from '../common/string-array-input';
+import CreateRestaurantSocial from './create-restaurant-social';
+
+const RestaurantContactReservationInfo = () => {
+  const { control, getValues, setFocus, setValue, watch } = useFormContext<ICreateRestaurant>();
+  const [reservation, setReservation] = useState<boolean>(!!getValues('details.reservation') || false);
+  const {
+    field: { value, onChange },
+  } = useController({
+    control,
+    name: 'details.paymentOptions',
+    rules: {
+      validate: (val) => {
+        const isValid = updateRestaurantSchema.shape.details.unwrap().shape.paymentOptions.safeParse(val);
+        return isValid.success || isValid.error.flatten().formErrors.join(', ');
+      },
+    },
+  });
+
+  const addNewSocial = () => {
+    const socials = getValues('contact.socialMedia') || [];
+    if (socials.find((s) => !s.name)) return toast.error('Use the previous social platform input');
+    setValue('contact.socialMedia', [...socials, { name: '', handle: '' }]);
+  };
+  const addSocial = (idx: number, social: ISocialLink) => {
+    const socials = getValues('contact.socialMedia') || [];
+    let newSocials = socials.toSpliced(idx, 1, social);
+    setValue('contact.socialMedia', newSocials);
+  };
+  const removeSocial = (idx: number) => {
+    const socials = getValues('contact.socialMedia') || [];
+    let newSocials = socials.toSpliced(idx, 1);
+    setValue('contact.socialMedia', newSocials);
+  };
+  return (
+    <div className="flex flex-col gap-4 pb-2 w-full">
+      <StringArrayInput
+        arr={value || []}
+        label="Add the payment methods accepted at your restaurant"
+        prevState={value || []}
+        setState={onChange}
+      />
+      <div className="grid grid-cols-3 gap-3">
+        <Controller
+          control={control}
+          render={({ field: { onChange, value }, fieldState: { error } }) => (
+            <Select
+              selectedKeys={value === undefined ? undefined : [value ? 'Yes' : 'No']}
+              onChange={(e) => onChange(e.target.value === 'Yes')}
+              isRequired
+              label="Do you have a delivery service?"
+              placeholder=" "
+              isInvalid={!!error}
+              errorMessage={error?.message}
+            >
+              <SelectItem key="Yes">Yes</SelectItem>
+              <SelectItem key="No">No</SelectItem>
+            </Select>
+          )}
+          name="details.delivery"
+          rules={{
+            validate: (val) => {
+              const isValid = updateRestaurantSchema.shape.details.unwrap().shape.delivery.safeParse(val);
+              return isValid.success || isValid.error.flatten().formErrors.join(', ');
+            },
+          }}
+        />
+        <Controller
+          control={control}
+          render={({ field: { onChange, value }, fieldState: { error } }) => (
+            <Select
+              selectedKeys={value === undefined ? undefined : [value ? 'Yes' : 'No']}
+              onChange={(e) => onChange(e.target.value === 'Yes')}
+              isRequired
+              label="Are children allowed?"
+              placeholder=" "
+              isInvalid={!!error}
+              errorMessage={error?.message}
+            >
+              <SelectItem key="Yes">Yes</SelectItem>
+              <SelectItem key="No">No</SelectItem>
+            </Select>
+          )}
+          name="details.children"
+          rules={{
+            validate: (val) => {
+              const isValid = updateRestaurantSchema.shape.details.unwrap().shape.children.safeParse(val);
+              return isValid.success || isValid.error.flatten().formErrors.join(', ');
+            },
+          }}
+        />
+        <Select
+          selectedKeys={[reservation ? 'Yes' : 'No']}
+          onChange={(e) => setReservation(e.target.value === 'Yes')}
+          label="Reservation service?"
+          placeholder=" "
+        >
+          <SelectItem key="Yes">Yes</SelectItem>
+          <SelectItem key="No">No</SelectItem>
+        </Select>
+      </div>
+      {reservation ? (
+        <div className="grid grid-cols-3 gap-3">
+          <Controller
+            control={control}
+            render={({ field: { onChange, ref, value }, fieldState: { error } }) => (
+              <Input
+                name="reservation_available"
+                label="No of tables available"
+                placeholder=" "
+                type="tel"
+                isRequired
+                value={value?.toString() || ''}
+                onValueChange={(val) => /^\d*$/.test(val) && onChange(+val)}
+                isInvalid={!!error}
+                errorMessage={error?.message}
+                ref={ref}
+                className="text-accentGray"
+              />
+            )}
+            name="details.reservation.available"
+            rules={{
+              validate: (val) => {
+                const isValid = updateRestaurantSchema.shape.details
+                  .unwrap()
+                  .shape.reservation.unwrap()
+                  .shape.available.safeParse(val);
+                return isValid.success || isValid.error.flatten().formErrors.join(', ');
+              },
+            }}
+          />
+          <Controller
+            control={control}
+            render={({ field: { onChange, ref, value }, fieldState: { error } }) => (
+              <Input
+                name="reservation_table"
+                label="Max guests per table"
+                placeholder=" "
+                type="tel"
+                isRequired
+                value={value?.toString() || ''}
+                onValueChange={(val) => /^\d*$/.test(val) && onChange(+val)}
+                isInvalid={!!error}
+                errorMessage={error?.message}
+                ref={ref}
+                className="text-accentGray"
+              />
+            )}
+            name="details.reservation.max"
+            rules={{
+              validate: (val) => {
+                const isValid = updateRestaurantSchema.shape.details
+                  .unwrap()
+                  .shape.reservation.unwrap()
+                  .shape.max.safeParse(val);
+                return isValid.success || isValid.error.flatten().formErrors.join(', ');
+              },
+            }}
+          />
+          <Controller
+            control={control}
+            render={({ field: { onChange, ref, value }, fieldState: { error } }) => (
+              <Input
+                name="reservation"
+                label="Table Price"
+                placeholder=" "
+                type="tel"
+                isRequired
+                value={value?.toString() || ''}
+                onValueChange={(val) => /^[\d.]*$/.test(val) && onChange(+val)}
+                isInvalid={!!error}
+                errorMessage={error?.message}
+                ref={ref}
+                className="text-accentGray"
+              />
+            )}
+            name="details.reservation.price"
+            rules={{
+              validate: (val) => {
+                const isValid = updateRestaurantSchema.shape.details
+                  .unwrap()
+                  .shape.reservation.unwrap()
+                  .shape.price.safeParse(val);
+                return isValid.success || isValid.error.flatten().formErrors.join(', ');
+              },
+            }}
+          />
+        </div>
+      ) : null}
+
+      <Controller
+        control={control}
+        render={({ field: { onChange, ref, value }, fieldState: { error } }) => (
+          <Input
+            name="email"
+            label="Email address"
+            labelPlacement="outside"
+            placeholder=" "
+            radius="full"
+            isRequired
+            value={value}
+            onChange={onChange}
+            onKeyDown={(e) => onEnter(e, () => setFocus('contact.phone'))}
+            isInvalid={!!error}
+            errorMessage={error?.message}
+            ref={ref}
+            className="text-accentGray"
+          />
+        )}
+        name="contact.email"
+        rules={{
+          validate: (val) => {
+            const isValid = updateRestaurantSchema.shape.contact.unwrap().shape.email.safeParse(val);
+            return isValid.success || isValid.error.flatten().formErrors.join(', ');
+          },
+        }}
+      />
+      <Controller
+        control={control}
+        render={({ field: { onChange, ref, value }, fieldState: { error } }) => (
+          <Input
+            name="phone"
+            label="Phone number"
+            labelPlacement="outside"
+            radius="full"
+            placeholder=" "
+            type="tel"
+            isRequired
+            value={value?.toString() || ''}
+            onValueChange={(val) => /^[0-9+()\s]*$/.test(val) && onChange(val)}
+            isInvalid={!!error}
+            errorMessage={error?.message}
+            ref={ref}
+            className="text-accentGray"
+          />
+        )}
+        name="contact.phone"
+        rules={{
+          validate: (val) => {
+            const isValid = updateRestaurantSchema.shape.contact.unwrap().shape.phone.safeParse(val);
+            return isValid.success || isValid.error.flatten().formErrors.join(', ');
+          },
+        }}
+      />
+      <div className="flex flex-col gap-2">
+        <div className="flex items-center justify-between gap-10">
+          <h6 className="text-accentGray text-sm font-semibold">Social Media Handle</h6>
+          <Button
+            className="text-xs font-medium pr-4"
+            color="primary"
+            radius="full"
+            size="sm"
+            variant="flat"
+            onPress={addNewSocial}
+            startContent={<IoAdd className="text-lg" />}
+          >
+            Add Social Media
+          </Button>
+        </div>
+        <div className="grid grid-cols-2 gap-6">
+          {(watch('contact.socialMedia') || []).map((social, idx) => (
+            <CreateRestaurantSocial
+              {...social}
+              key={social.name}
+              addSocial={(social: ISocialLink) => addSocial(idx, social)}
+              removeSocial={() => removeSocial(idx)}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantContactReservationInfo;
--- src/components/admin/restaurant/update-restaurant-info.tsx
+++ src/components/admin/restaurant/update-restaurant-info.tsx
@@ -1,4 +1,16 @@
-import { IRestaurant } from '@/types';
+'use client';
+
+import { StringArrayInput } from '@/components';
+import { updateRestaurant } from '@/server';
+import { DayOfWeek, IRestaurant, IUpdateRestaurant } from '@/types';
+import { getObjDiff } from '@/utils';
+import { parseAbsoluteToLocal } from '@internationalized/date';
+import { Button, Select, SelectItem, TimeInput, TimeInputValue } from '@nextui-org/react';
+import dayjs from 'dayjs';
+import { useState } from 'react';
+import { FormProvider, SubmitHandler, useController, useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import RestaurantContactReservationInfo from './contact-reservation-info';
 
 interface Props {
   restaurant: IRestaurant;
@@ -6,7 +18,131 @@ interface Props {
 }
 
 const UpdateRestaurantInfo = ({ restaurant, onClose }: Props) => {
-  return <div>UpdateRestaurantInfo</div>;
+  const method = useForm<IUpdateRestaurant>({ defaultValues: restaurant, mode: 'onChange' });
+  const { control, getValues, handleSubmit, reset, setValue } = method;
+  const avails = (getValues('availability') || []).map((a, i) => (a ? i : 7)).filter((p) => p < 7);
+  const [openAvails, setOpenAvails] = useState<number[]>(avails);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const {
+    field: { onChange: setServiceStyle, value: serviceStyle },
+  } = useController({ control, name: 'serviceStyle' });
+  const {
+    field: { onChange: setCuisines, value: cuisines },
+  } = useController({ control, name: 'cuisine' });
+  const {
+    field: { onChange: setDietaries, value: dietaries },
+  } = useController({ control, name: 'dietaryProvisions' });
+
+  const openCloseAvail = (idx: number, state: string) => {
+    state === 'close' && setValue(`availability.${idx}`, null);
+    setOpenAvails((prev) => prev.filter((p) => p !== idx).concat(state === 'close' ? [] : [idx]));
+  };
+  const setAvailability = (time: TimeInputValue, idx: number, field: 'from' | 'to') => {
+    const timeString = `${time.hour.toString().padStart(2, '0')}:${time.minute.toString().padStart(2, '0')}`;
+    setValue(`availability.${idx}.day`, Object.values(DayOfWeek)[idx]);
+    setValue(`availability.${idx}.${field}`, timeString);
+  };
+  const onSubmit: SubmitHandler<IUpdateRestaurant> = async (data) => {
+    setIsLoading(true);
+    try {
+      const updateBody = getObjDiff(data, restaurant);
+      if (!Object.keys(updateBody).length) {
+        onClose();
+        return toast.error('No change has been made!');
+      }
+      await updateRestaurant(updateBody, restaurant._id);
+      onClose();
+      reset();
+      toast.success('Stay rules updated successfully!');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <FormProvider {...method}>
+      <section className="flex flex-col gap-6 p-2">
+        <h3 className="text-2xl text-center font-semibold tracking-wide border-b py-2">Update Property Info</h3>
+        <article className="flex flex-col gap-4">
+          <div className="flex flex-col gap-2">
+            <h6 className="text-lg font-medium">Availability</h6>
+            <div className="grid grid-cols-2 gap-4">
+              {Object.values(DayOfWeek).map((item, i) => {
+                const [from, to] = getValues([`availability.${i}.from`, `availability.${i}.to`]);
+                const fromValue = from ? parseAbsoluteToLocal(dayjs(`2000-01-01 ${from}`).toISOString()) : undefined;
+                const toValue = to ? parseAbsoluteToLocal(dayjs(`2000-01-01 ${to}`).toISOString()) : undefined;
+                return (
+                  <div key={item} className="flex flex-col gap-2">
+                    <div className="flex items-center justify-between gap-4">
+                      <p className="text-sm text-black/60 font-medium">{item}</p>
+                      <Select
+                        selectedKeys={[openAvails.includes(i) ? 'open' : 'close']}
+                        onChange={(e) => openCloseAvail(i, e.target.value)}
+                        isRequired
+                        placeholder="Close"
+                        aria-label={`${item} availability`}
+                        className="w-24"
+                      >
+                        <SelectItem key="open">Open</SelectItem>
+                        <SelectItem key="close">Close</SelectItem>
+                      </Select>
+                    </div>
+                    {openAvails.includes(i) ? (
+                      <div className="grid grid-cols-2 gap-2">
+                        <TimeInput
+                          value={fromValue}
+                          onChange={(val) => setAvailability(val, i, 'from')}
+                          label="Open From"
+                          hideTimeZone
+                        />
+                        <TimeInput
+                          value={toValue}
+                          onChange={(val) => setAvailability(val, i, 'to')}
+                          label="Close At"
+                          hideTimeZone
+                        />
+                      </div>
+                    ) : null}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <StringArrayInput
+            arr={serviceStyle || []}
+            label="Service Style"
+            placeholder="e.g. Fast-food, Drive-through"
+            prevState={serviceStyle || []}
+            setState={setServiceStyle}
+          />
+          <StringArrayInput
+            arr={cuisines || []}
+            label="Cuisines"
+            placeholder="e.g. Chinese, Italian"
+            prevState={cuisines || []}
+            setState={setCuisines}
+          />
+          <StringArrayInput
+            arr={dietaries || []}
+            label="Dietary Provisions"
+            placeholder="e.g. Vegetarian, Gluten-free"
+            prevState={dietaries || []}
+            setState={setDietaries}
+          />
+          <RestaurantContactReservationInfo />
+          <Button
+            className="text-sm font-semibold px-14 py-6 my-4"
+            color="primary"
+            radius="full"
+            variant="solid"
+            onPress={() => handleSubmit(onSubmit)()}
+            isLoading={isLoading}
+          >
+            Update Restaurant Info
+          </Button>
+        </article>
+      </section>
+    </FormProvider>
+  );
 };
 
 export default UpdateRestaurantInfo;
