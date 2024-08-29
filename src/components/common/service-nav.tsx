83b4243d06b14939a56598b91a204192f2a74072
Toby2507 <oluwatobisalau2000@gmail.com>
2024-08-29 06:36:10 +0100

    Update the service nav to indicate and change active tab

--- src/components/common/service-nav.tsx
+++ src/components/common/service-nav.tsx
@@ -1,20 +1,37 @@
+'use client';
+
+import { useSearchStore } from '@/providers';
+import { NavTabs } from '@/types';
 import { Button } from '@nextui-org/react';
 import { IoBedOutline, IoRestaurantOutline } from 'react-icons/io5';
 import { PiWine } from 'react-icons/pi';
 import { RiShareCircleLine } from 'react-icons/ri';
 
-const services = [
-  { name: 'Stays', icon: <IoBedOutline /> },
-  { name: 'Restaurants', icon: <IoRestaurantOutline /> },
-  { name: 'Night Life', icon: <PiWine /> },
-  { name: 'Airtime Topup', icon: <RiShareCircleLine /> },
+interface IServices {
+  name: string;
+  key: NavTabs;
+  icon: JSX.Element;
+}
+const services: IServices[] = [
+  { name: 'Stays', key: 'Stay', icon: <IoBedOutline /> },
+  { name: 'Restaurants', key: 'Restaurant', icon: <IoRestaurantOutline /> },
+  { name: 'Night Life', key: 'Nightlife', icon: <PiWine /> },
+  { name: 'Airtime Topup', key: 'Airtime', icon: <RiShareCircleLine /> },
 ];
 
 const ServiceNav = () => {
+  const { activeTab, setActiveTab } = useSearchStore();
   return (
     <ul className="flex items-center justify-center flex-wrap gap-4 pb-16">
-      {services.map(({ name, icon }, idx) => (
-        <Button key={`${name}-${idx}`} className="text-white" startContent={icon} radius="full" variant="light">
+      {services.map(({ name, icon, key }) => (
+        <Button
+          key={key}
+          className={`text-white ${key === activeTab ? 'border border-white bg-white/10' : ''}`}
+          onPress={() => setActiveTab(key)}
+          startContent={icon}
+          radius="full"
+          variant="light"
+        >
           {name}
         </Button>
       ))}
