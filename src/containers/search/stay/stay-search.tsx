f15be3ffaea2a3c28f7bcb8b5a2cc7a2ad4b1113
Toby2507 <oluwatobisalau2000@gmail.com>
2024-08-29 11:02:57 +0100

    Add the stay search page search result

--- src/containers/search/stay/stay-search.tsx
+++ src/containers/search/stay/stay-search.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { SearchStayCard } from '@/components';
 import { StaySearchBar } from '@/containers';
 import { useSearchStore } from '@/providers';
 import { getStaySearch, refetchPage } from '@/server';
@@ -24,17 +25,37 @@ const StaySearchPage = ({ searchParam }: Props) => {
       noOfGuests.adults + noOfGuests.children,
       reservationCount
     );
-    console.log(stays);
     setSearchResult(stays);
   };
   useEffect(() => {
     if (!searchResult && location) (async () => await searchStay())();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [location, searchResult]);
-  if (!location) return null;
   return (
     <>
       <StaySearchBar extraClass="-mt-7" search={() => refetchPage(paths.searchStay(searchParam))} />
+      <div className="grid grid-cols-10 gap-4 pb-10">
+        <div className="col-span-3 flex flex-col border rounded-xl">
+          <h3 className="text-lg font-bold border-b px-3 py-4">Filter by: </h3>
+          <div className="flex flex-col gap-2 px-4 py-2">
+            <h4 className="text-base font-bold">Stay type</h4>
+          </div>
+        </div>
+        <div className="col-span-7 flex flex-col gap-4">
+          <h1 className="text-xl font-bold">
+            {location?.fullAddress}: {searchResult?.length || 0} properties found
+          </h1>
+          <div className="flex flex-col gap-4">
+            {searchResult ? (
+              searchResult.map((stay) => <SearchStayCard key={stay._id} {...stay} />)
+            ) : (
+              <div className="grid place-items-center h-72">
+                <p className="text-sm text-accentGray font-medium">No Properties found</p>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
