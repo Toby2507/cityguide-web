e360b929e267edfe096d7d4fd3366539d5eaba3f
Toby2507 <oluwatobisalau2000@gmail.com>
2024-08-29 08:33:19 +0100

    Update the search page implementation

--- src/app/search/stay/[[...searchparams]]/page.tsx
+++ src/app/search/stay/[[...searchparams]]/page.tsx
@@ -1,4 +1,4 @@
-import { Footer, Header } from '@/containers';
+import { Footer, Header, StaySearch } from '@/containers';
 
 interface Props {
   params: {
@@ -10,7 +10,11 @@ const StaySearchPage = ({ params: { searchparams } }: Props) => {
   return (
     <>
       <Header />
-      <main className="container flex flex-col gap-6 bg-white max-w-7xl px-10 mx-auto"></main>
+      <main className="flex flex-col gap-20 bg-white">
+        <div className="container mx-auto px-10 flex flex-col gap-6 max-w-7xl h-44">
+          <StaySearch searchParam={searchparams} />
+        </div>
+      </main>
       <Footer />
     </>
   );
