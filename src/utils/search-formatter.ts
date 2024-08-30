c21e8c43b72a31d29bd5ccb5e1a37f03f90b950f
Toby2507 <oluwatobisalau2000@gmail.com>
2024-08-30 16:51:01 +0100

    Add the optimized filter functions and refactor the get max price function to retrieve both the min and max

--- src/utils/search-formatter.ts
+++ src/utils/search-formatter.ts
@@ -4,6 +4,8 @@ interface IFilter {
   [key: string]: number;
 }
 
+// Stay
+// // Data
 export const stayTypeFormat = (stays: IStay[]): IFilter => {
   return stays.reduce((types: IFilter, stay) => {
     types[stay.type] = (types[stay.type] || 0) + 1;
@@ -11,8 +13,11 @@ export const stayTypeFormat = (stays: IStay[]): IFilter => {
   }, {});
 };
 
-export const getMaxPrice = (stays: IStay[]): number =>
-  Math.max(...stays.flatMap((stay) => stay.accommodation.map((acc) => acc.price)));
+export const getMinMaxPrice = (stays: IStay[]) => {
+  if (!stays.length) return { min: 0, max: 1000 };
+  const flatAcc = stays.flatMap((stay) => stay.accommodation.map((acc) => acc.price));
+  return { min: Math.min(...flatAcc), max: Math.max(...flatAcc) };
+};
 
 export const ratingsFormat = (stays: IStay[]): IFilter => {
   const ratingRange = [
@@ -71,7 +76,8 @@ export const paymentFormat = (stays: IStay[]): IFilter => {
   for (const stay of stays) {
     for (const pay of stay.paymentMethods) {
       const payment = `${pay[0].toUpperCase()}${pay.slice(1).toLowerCase()}`;
-      if (payment.includes('Credit') || payment.includes('Debit')) payments['Credit/Debit Cards'] += 1;
+      if (payment.toLowerCase().includes('credit') || payment.toLowerCase().includes('debit'))
+        payments['Credit/Debit Cards'] += 1;
       else payments[payment] = (payments[payment] || 0) + 1;
     }
   }
@@ -91,7 +97,7 @@ export const policyFormat = (stays: IStay[]): IFilter => {
 export const getFilterData = (stays: IStay[]) => {
   return {
     stayTypes: stayTypeFormat(stays),
-    maxPrice: getMaxPrice(stays),
+    maxPrice: getMinMaxPrice(stays),
     ratings: ratingsFormat(stays),
     maxDays: maxDaysFormat(stays),
     languages: languageFormat(stays),
@@ -99,4 +105,86 @@ export const getFilterData = (stays: IStay[]) => {
     payments: paymentFormat(stays),
     policies: policyFormat(stays),
   };
+};
+// // Filters
+export const filterByType = (stays: IStay[], filterBy: Set<string>) => {
+  return stays.filter((stay) => filterBy.has(stay.type));
+};
+
+export const filterByRating = (stays: IStay[], filterBy: string) => {
+  const minRating = +filterBy.split(': ')[1].replace('+', '');
+  return stays.filter((stay) => stay.rating >= minRating);
+};
+
+export const filterByMaxDays = (stays: IStay[], filterBy: string) => {
+  const maxDays = +filterBy.split(' ')[0];
+  return stays.filter((stay) => stay.maxDays === maxDays);
+};
+
+export const filterByLanguage = (stays: IStay[], filterBy: string[]) =>
+  stays.filter((stay) => {
+    const langs = new Set(stay.language);
+    return filterBy.every((lang) => langs.has(`${lang[0].toUpperCase()}${lang.slice(1).toLowerCase()}`));
+  });
+
+export const filterByDistance = (stays: IStay[], filterBy: string): IStay[] => {
+  const [op, _, d, __] = filterBy.split(' ');
+  let maxDistance = Infinity;
+  if (op === 'Less') maxDistance = +d * 1000;
+  if (maxDistance === Infinity) return stays;
+  return stays.filter((stay) => (stay.locationInfo?.distance ?? 21000) <= maxDistance);
+};
+
+export const filterByPayment = (stays: IStay[], filterBy: string[]) => {
+  return stays.filter((stay) => {
+    const paySet = new Set(stay.paymentMethods.map((pay) => `${pay[0].toUpperCase()}${pay.slice(1).toLowerCase()}`));
+    return filterBy.every((pay) => {
+      if (
+        pay === 'Credit/Debit Cards' &&
+        stay.paymentMethods.some((p) => p.toLowerCase().includes('credit') || p.toLowerCase().includes('debit'))
+      )
+        return true;
+      return paySet.has(pay);
+    });
+  });
+};
+
+export const filterByPolicy = (stays: IStay[], filterBy: string) => {
+  return stays.filter((stay) => {
+    if (filterBy === 'Free cancellation' && !(stay.partner as IPartner).cancellationPolicy) return true;
+    if (filterBy === 'No prepayment' && ![StayType.APARTMENT, StayType.BnB].includes(stay.type)) return true;
+    if (
+      filterBy === 'Reserve without credit card' &&
+      stay.paymentMethods.some((p) => p.toLowerCase().includes('credit'))
+    )
+      return true;
+    return false;
+  });
+};
+
+export const filterByPrice = (stays: IStay[], min: number, max: number) =>
+  stays.filter((stay) => stay.accommodation.some((acc) => acc.price >= min && acc.price <= max));
+
+export const filterStayResults = (
+  stays: IStay[],
+  type: string[],
+  rating: string,
+  maxdays: string,
+  language: string[],
+  distance: string,
+  payment: string[],
+  policy: string,
+  min: number,
+  max: number
+) => {
+  let results = [...stays];
+  results = filterByPrice(results, min, max);
+  if (type.length) results = filterByType(results, new Set(type));
+  if (rating) results = filterByRating(results, rating);
+  if (maxdays) results = filterByMaxDays(results, maxdays);
+  if (language.length) results = filterByLanguage(results, language);
+  if (distance) results = filterByDistance(results, distance);
+  if (payment.length) results = filterByPayment(results, payment);
+  if (policy) results = filterByPolicy(results, policy);
+  return results;
 };
