78c27b4aaa8af219ef9cb137cb426465630c7748
Toby2507 <oluwatobisalau2000@gmail.com>
2024-08-30 11:29:53 +0100

    Add the payments and policies filter box and add separate states for each filter operations

--- src/components/search/stay/stay-filter-box.tsx
+++ src/components/search/stay/stay-filter-box.tsx
@@ -12,28 +12,39 @@ interface Props {
 
 const SearchStayFilterBox = ({ stays, filterStays }: Props) => {
   const [selectedType, setSelectedType] = useState<string[]>([]);
+  const [rating, setRating] = useState<string[]>([]);
+  const [maxdays, setMaxdays] = useState<string[]>([]);
+  const [language, setLanguage] = useState<string[]>([]);
+  const [distance, setDistance] = useState<string[]>([]);
+  const [payment, setPayment] = useState<string[]>([]);
+  const [policy, setPolicy] = useState<string[]>([]);
   const [priceRange, setPriceRange] = useState<SliderValue>([0, 500000]);
 
-  const { stayTypes, maxPrice, ratings, maxDays, languages, distances } = useMemo(() => getFilterData(stays), [stays]);
+  const { stayTypes, maxPrice, ratings, maxDays, languages, distances, payments, policies } = useMemo(
+    () => getFilterData(stays),
+    [stays]
+  );
   return (
     <>
-      <div className="flex flex-col gap-2 border-b px-4 pt-2 pb-4">
-        <CheckboxGroup
-          classNames={{ label: '!text-sm text-black font-semibold' }}
-          label="Stay Type: "
-          onValueChange={setSelectedType}
-          value={selectedType}
-        >
-          {Object.entries(stayTypes).map(([label, count]) => (
-            <Checkbox classNames={{ base: 'max-w-full', label: 'w-full' }} key={label} value={label}>
-              <div className="flex items-center justify-between gap-2 w-full">
-                <p className="text-sm font-normal tracking-wide">{label}</p>
-                <p className="text-sm font-light">{count}</p>
-              </div>
-            </Checkbox>
-          ))}
-        </CheckboxGroup>
-      </div>
+      {Object.values(stayTypes).length ? (
+        <div className="flex flex-col gap-2 border-b px-4 pt-2 pb-4">
+          <CheckboxGroup
+            classNames={{ label: '!text-sm text-black font-semibold' }}
+            label="Stay Type: "
+            onValueChange={setSelectedType}
+            value={selectedType}
+          >
+            {Object.entries(stayTypes).map(([label, count]) => (
+              <Checkbox classNames={{ base: 'max-w-full', label: 'w-full' }} key={label} value={label}>
+                <div className="flex items-center justify-between gap-2 w-full">
+                  <p className="text-sm font-normal tracking-wide">{label}</p>
+                  <p className="text-sm font-light">{count}</p>
+                </div>
+              </Checkbox>
+            ))}
+          </CheckboxGroup>
+        </div>
+      ) : null}
       <div className="flex flex-col gap-6 border-b px-4 pt-2 pb-4">
         <h4 className="text-sm text-black font-semibold">Your budget (per night):</h4>
         <Slider
@@ -47,74 +58,120 @@ const SearchStayFilterBox = ({ stays, filterStays }: Props) => {
           className="max-w-md"
         />
       </div>
-      <div className="flex flex-col gap-2 border-b px-4 pt-2 pb-4">
-        <CheckboxGroup
-          classNames={{ label: '!text-sm text-black font-semibold' }}
-          label="Rating: "
-          onValueChange={setSelectedType}
-          value={selectedType}
-        >
-          {Object.entries(ratings).map(([label, count]) => (
-            <Checkbox classNames={{ base: 'max-w-full', label: 'w-full' }} key={label} value={label}>
-              <div className="flex items-center justify-between gap-2 w-full">
-                <p className="text-sm font-normal tracking-wide">{label}</p>
-                <p className="text-sm font-light">{count}</p>
-              </div>
-            </Checkbox>
-          ))}
-        </CheckboxGroup>
-      </div>
-      <div className="flex flex-col gap-2 border-b px-4 pt-2 pb-4">
-        <CheckboxGroup
-          classNames={{ label: '!text-sm text-black font-semibold' }}
-          label="Max Reservation Days: "
-          onValueChange={setSelectedType}
-          value={selectedType}
-        >
-          {Object.entries(maxDays).map(([label, count]) => (
-            <Checkbox classNames={{ base: 'max-w-full', label: 'w-full' }} key={label} value={label}>
-              <div className="flex items-center justify-between gap-2 w-full">
-                <p className="text-sm font-normal tracking-wide">{label}</p>
-                <p className="text-sm font-light">{count}</p>
-              </div>
-            </Checkbox>
-          ))}
-        </CheckboxGroup>
-      </div>
-      <div className="flex flex-col gap-2 border-b px-4 pt-2 pb-4">
-        <CheckboxGroup
-          classNames={{ label: '!text-sm text-black font-semibold' }}
-          label="Staff Spoken Language: "
-          onValueChange={setSelectedType}
-          value={selectedType}
-        >
-          {Object.entries(languages).map(([label, count]) => (
-            <Checkbox classNames={{ base: 'max-w-full', label: 'w-full' }} key={label} value={label}>
-              <div className="flex items-center justify-between gap-2 w-full">
-                <p className="text-sm font-normal tracking-wide">{label}</p>
-                <p className="text-sm font-light">{count}</p>
-              </div>
-            </Checkbox>
-          ))}
-        </CheckboxGroup>
-      </div>
-      <div className="flex flex-col gap-2 px-4 pt-2 pb-4">
-        <CheckboxGroup
-          classNames={{ label: '!text-sm text-black font-semibold' }}
-          label="Distance from selected address: "
-          onValueChange={setSelectedType}
-          value={selectedType}
-        >
-          {Object.entries(distances).map(([label, count]) => (
-            <Checkbox classNames={{ base: 'max-w-full', label: 'w-full' }} key={label} value={label}>
-              <div className="flex items-center justify-between gap-2 w-full">
-                <p className="text-sm font-normal tracking-wide">{label}</p>
-                <p className="text-sm font-light">{count}</p>
-              </div>
-            </Checkbox>
-          ))}
-        </CheckboxGroup>
-      </div>
+      {Object.values(ratings).length ? (
+        <div className="flex flex-col gap-2 border-b px-4 pt-2 pb-4">
+          <CheckboxGroup
+            classNames={{ label: '!text-sm text-black font-semibold' }}
+            label="Rating: "
+            onValueChange={setRating}
+            value={rating}
+          >
+            {Object.entries(ratings).map(([label, count]) => (
+              <Checkbox classNames={{ base: 'max-w-full', label: 'w-full' }} key={label} value={label}>
+                <div className="flex items-center justify-between gap-2 w-full">
+                  <p className="text-sm font-normal tracking-wide">{label}</p>
+                  <p className="text-sm font-light">{count}</p>
+                </div>
+              </Checkbox>
+            ))}
+          </CheckboxGroup>
+        </div>
+      ) : null}
+      {Object.values(maxDays).length ? (
+        <div className="flex flex-col gap-2 border-b px-4 pt-2 pb-4">
+          <CheckboxGroup
+            classNames={{ label: '!text-sm text-black font-semibold' }}
+            label="Max Reservation Days: "
+            onValueChange={setMaxdays}
+            value={maxdays}
+          >
+            {Object.entries(maxDays).map(([label, count]) => (
+              <Checkbox classNames={{ base: 'max-w-full', label: 'w-full' }} key={label} value={label}>
+                <div className="flex items-center justify-between gap-2 w-full">
+                  <p className="text-sm font-normal tracking-wide">{label}</p>
+                  <p className="text-sm font-light">{count}</p>
+                </div>
+              </Checkbox>
+            ))}
+          </CheckboxGroup>
+        </div>
+      ) : null}
+      {Object.values(languages).length ? (
+        <div className="flex flex-col gap-2 border-b px-4 pt-2 pb-4">
+          <CheckboxGroup
+            classNames={{ label: '!text-sm text-black font-semibold' }}
+            label="Staff Spoken Language: "
+            onValueChange={setLanguage}
+            value={language}
+          >
+            {Object.entries(languages).map(([label, count]) => (
+              <Checkbox classNames={{ base: 'max-w-full', label: 'w-full' }} key={label} value={label}>
+                <div className="flex items-center justify-between gap-2 w-full">
+                  <p className="text-sm font-normal tracking-wide">{label}</p>
+                  <p className="text-sm font-light">{count}</p>
+                </div>
+              </Checkbox>
+            ))}
+          </CheckboxGroup>
+        </div>
+      ) : null}
+      {Object.values(distances).length ? (
+        <div className="flex flex-col gap-2 border-b px-4 pt-2 pb-4">
+          <CheckboxGroup
+            classNames={{ label: '!text-sm text-black font-semibold' }}
+            label="Distance from selected address: "
+            onValueChange={setDistance}
+            value={distance}
+          >
+            {Object.entries(distances).map(([label, count]) => (
+              <Checkbox classNames={{ base: 'max-w-full', label: 'w-full' }} key={label} value={label}>
+                <div className="flex items-center justify-between gap-2 w-full">
+                  <p className="text-sm font-normal tracking-wide">{label}</p>
+                  <p className="text-sm font-light">{count}</p>
+                </div>
+              </Checkbox>
+            ))}
+          </CheckboxGroup>
+        </div>
+      ) : null}
+      {Object.values(payments).length ? (
+        <div className="flex flex-col gap-2 border-b px-4 pt-2 pb-4">
+          <CheckboxGroup
+            classNames={{ label: '!text-sm text-black font-semibold' }}
+            label="Payment methods: "
+            onValueChange={setPayment}
+            value={payment}
+          >
+            {Object.entries(payments).map(([label, count]) => (
+              <Checkbox classNames={{ base: 'max-w-full', label: 'w-full' }} key={label} value={label}>
+                <div className="flex items-center justify-between gap-2 w-full">
+                  <p className="text-sm font-normal tracking-wide">{label}</p>
+                  <p className="text-sm font-light">{count}</p>
+                </div>
+              </Checkbox>
+            ))}
+          </CheckboxGroup>
+        </div>
+      ) : null}
+      {Object.values(policies).length ? (
+        <div className="flex flex-col gap-2 px-4 pt-2 pb-4">
+          <CheckboxGroup
+            classNames={{ label: '!text-sm text-black font-semibold' }}
+            label="Reservation Policy: "
+            onValueChange={setPolicy}
+            value={policy}
+          >
+            {Object.entries(policies).map(([label, count]) => (
+              <Checkbox classNames={{ base: 'max-w-full', label: 'w-full' }} key={label} value={label}>
+                <div className="flex items-center justify-between gap-2 w-full">
+                  <p className="text-sm font-normal tracking-wide">{label}</p>
+                  <p className="text-sm font-light">{count}</p>
+                </div>
+              </Checkbox>
+            ))}
+          </CheckboxGroup>
+        </div>
+      ) : null}
     </>
   );
 };
