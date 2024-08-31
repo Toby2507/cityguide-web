41d495ff8a44af19cf0054a8b32728ca6e0137c2
Toby2507 <oluwatobisalau2000@gmail.com>
2024-08-31 17:42:55 +0100

    Update the service nav handle navigation function

--- src/components/common/service-nav.tsx
+++ src/components/common/service-nav.tsx
@@ -3,6 +3,7 @@
 import { useSearchStore } from '@/providers';
 import { NavTabs } from '@/types';
 import { Button } from '@nextui-org/react';
+import { useRouter } from 'next/navigation';
 import { IoBedOutline, IoRestaurantOutline } from 'react-icons/io5';
 import { PiWine } from 'react-icons/pi';
 import { RiShareCircleLine } from 'react-icons/ri';
@@ -21,13 +22,19 @@ const services: IServices[] = [
 
 const ServiceNav = () => {
   const { activeTab, setActiveTab } = useSearchStore();
+  const { push } = useRouter();
+
+  const handleNav = (key: NavTabs) => {
+    setActiveTab(key);
+    push(`/${key.toLowerCase()}`);
+  };
   return (
     <ul className="flex items-center justify-center flex-wrap gap-4 pb-16">
       {services.map(({ name, icon, key }) => (
         <Button
           key={key}
           className={`text-white ${key === activeTab ? 'border border-white bg-white/10' : ''}`}
-          onPress={() => setActiveTab(key)}
+          onPress={() => handleNav(key)}
           startContent={icon}
           radius="full"
           variant="light"
