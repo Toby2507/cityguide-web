8aadecb9f756fdc862d57a9ba024f301130d7794
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-01 17:52:24 +0100

    Fix hydration error by rendering the search bar after the dom has been updated

--- src/containers/home/restaurant/trending-destinations.tsx
+++ src/containers/home/restaurant/trending-destinations.tsx
@@ -6,9 +6,13 @@ const RestaurantTrendingDestination = async () => {
   const restaurants = await getTrendingRestaurants();
   return (
     <section className="flex flex-col gap-4">
-      <SectionHeader title="Trending destinations" desc="Most popular choices for travelers from Nigeria" viewUrl="#" />
+      <SectionHeader
+        title="Trending destinations"
+        desc="Most popular choices for travelers from Nigeria"
+        viewUrl={restaurants.length > 6 ? '#' : ''}
+      />
       <div className="grid grid-cols-3 gap-4 px-2">
-        {restaurants?.map((place) => (
+        {restaurants?.slice(0, 6).map((place) => (
           <PlaceCard key={place._id} {...place} refPath={paths.restaurantDetail} />
         ))}
       </div>
