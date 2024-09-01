1676d342ddb2c70bf6f46c26d5e5d88c482c97e0
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-01 17:53:31 +0100

    Refactor the trending destination section to render atmost 6 destinations and show the veiw more button only if there are more than 6 trending destinations

--- src/containers/home/stay/trending-destination.tsx
+++ src/containers/home/stay/trending-destination.tsx
@@ -6,9 +6,13 @@ const TrendingDestinations = async () => {
   const stays = await getTrendingStays();
   return (
     <section className="flex flex-col gap-4">
-      <SectionHeader title="Trending destinations" desc="Most popular choices for travelers from Nigeria" viewUrl="#" />
+      <SectionHeader
+        title="Trending destinations"
+        desc="Most popular choices for travelers from Nigeria"
+        viewUrl={stays.length > 6 ? '#' : ''}
+      />
       <div className="grid grid-cols-3 gap-4 px-2">
-        {stays?.map((place) => (
+        {stays?.slice(0, 6).map((place) => (
           <PlaceCard key={place._id} {...place} refPath={paths.stayDetail} />
         ))}
       </div>
