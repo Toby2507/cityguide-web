37f44c8809ae9a5690d135ecd11febe50d780060
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-01 09:47:31 +0100

    Add the restaurant detail page implementation for the user side

--- src/app/restaurant/[resId]/page.tsx
+++ src/app/restaurant/[resId]/page.tsx
@@ -1,11 +1,38 @@
+import {
+  DetailPageAmenities,
+  DetailPageOverview,
+  Footer,
+  Header,
+  RestaurantDetailInfo,
+  RestaurantDetailMenu,
+  SubscribeBox,
+} from '@/containers';
+import { getRestaurantById } from '@/server';
+
 interface Props {
   params: {
     resId: string;
   };
 }
 
-const RestaurantDetailPage = ({ params: { resId } }: Props) => {
-  return <div>RestaurantDetailPage</div>;
+const RestaurantDetailPage = async ({ params: { resId } }: Props) => {
+  const restaurant = await getRestaurantById(resId);
+  if (!restaurant) return null;
+  return (
+    <>
+      <Header />
+      <main className="flex flex-col gap-20 bg-white">
+        <div className="container mx-auto px-10 flex flex-col gap-6 max-w-7xl">
+          <DetailPageOverview amenities={restaurant.details.amenities} {...restaurant} />
+          <DetailPageAmenities amenities={restaurant.details.amenities} {...restaurant} />
+          <RestaurantDetailMenu menu={restaurant.menu} />
+          <RestaurantDetailInfo restaurant={restaurant} />
+        </div>
+        <SubscribeBox />
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default RestaurantDetailPage;
