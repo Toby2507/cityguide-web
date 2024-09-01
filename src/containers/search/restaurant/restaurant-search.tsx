773ae8a0671465580b9dc5bc4f9ddc8f094d9a41
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-01 19:41:28 +0100

    Add the restaurant search page ui

--- src/containers/search/restaurant/restaurant-search.tsx
+++ src/containers/search/restaurant/restaurant-search.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { SearchCardLoader, SearchRestaurantCard } from '@/components';
+import { RestaurantSearchBar } from '@/containers';
 import { useSearchStore } from '@/providers';
 import { getRestaurantSearch } from '@/server';
 import { IRestaurant } from '@/types';
@@ -35,7 +37,38 @@ const RestaurantSearchPage = ({ searchParam }: Props) => {
     if (!searchResult && (location || searchParam)) (async () => await searchRestaurant())();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  return <div>RestaurantSearchPage</div>;
+  return (
+    <>
+      <RestaurantSearchBar extraClass="-mt-7" search={searchRestaurant} />
+      <div className="grid grid-cols-10 items-start gap-4 pb-10">
+        <div className="col-span-3 flex flex-col border rounded-xl">
+          <h3 className="text-lg font-bold border-b px-3 py-4">Filter by: </h3>
+          {/* <SearchStayFilterBox stays={searchResult} types={searchParam} filterStays={setFilteredResult} /> */}
+        </div>
+        <div className="col-span-7 flex flex-col gap-4">
+          {isLoading ? (
+            <SearchCardLoader />
+          ) : (
+            <>
+              <h1 className="text-xl font-bold">
+                {location ? `${location.fullAddress}: ` : ''}
+                {filteredResult?.length || 0} properties found
+              </h1>
+              <div className="flex flex-col gap-4">
+                {filteredResult ? (
+                  filteredResult.map((stay) => <SearchRestaurantCard key={stay._id} {...stay} />)
+                ) : (
+                  <div className="grid place-items-center h-72">
+                    <p className="text-sm text-accentGray font-medium">No Properties found</p>
+                  </div>
+                )}
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default RestaurantSearchPage;
