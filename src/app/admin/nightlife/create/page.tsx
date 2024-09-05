40197a2249e963eea67fd0da17be5430f0576f6d
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-05 06:31:18 +0100

    Add the create nightlife step 1 component to the create nightlife page

--- src/app/admin/nightlife/create/page.tsx
+++ src/app/admin/nightlife/create/page.tsx
@@ -1,5 +1,28 @@
+'use client';
+
+import { CreateNightlifeStep1 } from '@/components';
+import { ICreateNightlife } from '@/types';
+import { Pagination } from '@nextui-org/react';
+import { useEffect, useState } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+
 const CreateNightlifePage = () => {
-  return <div>CreateNightlifePage</div>;
+  const [step, setStep] = useState<number>(1);
+  const [topStep, setTopStep] = useState<number>(1);
+  const methods = useForm<ICreateNightlife>();
+
+  useEffect(() => {
+    setTopStep((prev) => Math.max(prev, step));
+  }, [step]);
+  return (
+    <FormProvider {...methods}>
+      {step === 1 ? <CreateNightlifeStep1 setStep={setStep} /> : null}
+      <div className="absolute bottom-10 flex flex-col gap-2">
+        <p className="text-xs">Go to step</p>
+        <Pagination className="z-50" total={topStep} page={step} onChange={(val) => setStep(val)} />
+      </div>
+    </FormProvider>
+  );
 };
 
 export default CreateNightlifePage;
