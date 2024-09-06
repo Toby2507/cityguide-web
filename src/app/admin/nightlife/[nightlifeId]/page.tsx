d3868c42ce98d3375b4c565ab8fcb2fdba76c427
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-06 10:35:39 +0100

    Add the nightlife info component and the update nightlife info component

--- src/app/admin/nightlife/[nightlifeId]/page.tsx
+++ src/app/admin/nightlife/[nightlifeId]/page.tsx
@@ -1,5 +1,16 @@
-const NightlifeDetailPage = () => {
-  return <div>NightlifeDetailPage</div>;
+import { AdminNightlifeDetail } from '@/containers';
+import { getNightlifeById } from '@/server';
+
+interface Props {
+  params: {
+    nightlifeId: string;
+  };
+}
+
+const NightlifeDetailPage = async ({ params: { nightlifeId } }: Props) => {
+  const nightlife = await getNightlifeById(nightlifeId);
+  if (!nightlife) return null;
+  return <AdminNightlifeDetail nightlife={nightlife} />;
 };
 
 export default NightlifeDetailPage;
