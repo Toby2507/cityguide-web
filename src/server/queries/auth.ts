c92f8a1833a8680fc19b2e0f49097193ba54da3c
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-06 05:41:28 +0100

    Update the auth queries

--- src/server/queries/auth.ts
+++ src/server/queries/auth.ts
@@ -5,17 +5,19 @@ import { fetchWithReAuth } from '@/utils';
 import { cookies } from 'next/headers';
 import toast from 'react-hot-toast';
 
+const BASEURL = process.env.NEXT_PUBLIC_API_URL;
+
 export const getUser = async () => {
-  const fullName = cookies().get('city-guide-username')?.value!;
-  const email = cookies().get('city-guide-useremail')?.value!;
-  const imgUrl = cookies().get('city-guide-userimg')?.value!;
-  const id = cookies().get('city-guide-userid')?.value!;
-  const type = cookies().get('city-guide-type')?.value! as EntityType;
-  const favProperties = cookies().get('city-guide-userfavproperties')?.value;
-  const phoneNumber = cookies().get('city-guide-userphone')?.value!;
-  let favouriteProperties: string[] | undefined;
+  const fullName = cookies().get('username')?.value;
+  const email = cookies().get('useremail')?.value!;
+  const imgUrl = cookies().get('userimg')?.value!;
+  const id = cookies().get('userid')?.value;
+  const type = cookies().get('type')?.value as EntityType;
+  const favProperties = cookies().get('userfavproperties')?.value;
+  const phoneNumber = cookies().get('userphone')?.value!;
+  let favouriteProperties: string[] = [];
   if (favProperties) favouriteProperties = JSON.parse(favProperties) as string[];
-  const partner = cookies().get('city-guide-partner')?.value;
+  const partner = cookies().get('partner')?.value;
   if (!fullName || !id || !partner) return null;
   const isPartner = partner === 'true' ? true : false;
   const user: IUserDetails = { id, fullName, email, phoneNumber, imgUrl, isPartner, type, favouriteProperties };
@@ -32,42 +34,40 @@ export const getUserProfile = async () => {
   return user.user as IUser;
 };
 
-export const setCookies = (res: any, type: EntityType) => {
+export const setCookies = async (res: any, type: EntityType) => {
   cookies().set('access-token', res.accessToken);
   cookies().set('refresh-token', res.refreshToken);
   cookies().set(
-    'city-guide-username',
+    'username',
     type === EntityType.USER ? `${res.user.firstName} ${res.user.lastName}` : res.establishment.name
   );
-  cookies().set('city-guide-useremail', type === EntityType.USER ? res.user.email : res.establishment.email);
-  cookies().set(
-    'city-guide-userphone',
-    type === EntityType.USER ? res.user.phoneNumber : res.establishment.phoneNumber
-  );
-  cookies().set('city-guide-userimg', type === EntityType.USER ? res.user.imgUrl : res.establishment.imgUrl);
-  cookies().set('city-guide-userid', type === EntityType.USER ? res.user._id : res.establishment._id);
-  cookies().set('city-guide-partner', type === EntityType.USER ? res.user.isPartner : true);
-  cookies().set('city-guide-type', type);
-  type === EntityType.USER &&
-    cookies().set('city-guide-userfavproperties', JSON.stringify(res.user.favouriteProperties));
+  cookies().set('useremail', type === EntityType.USER ? res.user.email : res.establishment.email);
+  cookies().set('userphone', type === EntityType.USER ? res.user.phoneNumber : res.establishment.phoneNumber);
+  cookies().set('userimg', type === EntityType.USER ? res.user.imgUrl : res.establishment.imgUrl);
+  cookies().set('userid', type === EntityType.USER ? res.user._id : res.establishment._id);
+  cookies().set('partner', type === EntityType.USER ? res.user.isPartner : true);
+  cookies().set('type', type);
+  type === EntityType.USER && cookies().set('userfavproperties', JSON.stringify(res.user.favouriteProperties));
 };
 
 export const upgradeToPartner = (res: any) => {
   cookies().set('access-token', res.accessToken);
-  cookies().set('city-guide-partner', 'true');
+  cookies().set('partner', 'true');
 };
 
 const _logout = async () => {
-  cookies().delete('city-guide-username');
-  cookies().delete('city-guide-useremail');
-  cookies().delete('city-guide-userphone');
-  cookies().delete('city-guide-userimg');
-  cookies().delete('city-guide-userid');
-  cookies().delete('city-guide-partner');
-  cookies().delete('city-guide-userfavproperties');
-  cookies().delete('city-guide-type');
+  cookies().delete('username');
+  cookies().delete('useremail');
+  cookies().delete('userphone');
+  cookies().delete('userimg');
+  cookies().delete('userid');
+  cookies().delete('partner');
+  cookies().delete('userfavproperties');
+  cookies().delete('type');
   cookies().delete('access-token');
   cookies().delete('refresh-token');
 };
 
-export const logout = async () => _logout();
+export const logout = async () => {
+  await _logout();
+};
