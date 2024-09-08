393262973f51f998d00ec1dcfa2213b022662cb1
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-08 11:38:46 +0100

    Update the nightlife categories section with the remaining nightlife categories

--- src/containers/home/nightlife/categories.tsx
+++ src/containers/home/nightlife/categories.tsx
@@ -1,7 +1,10 @@
 import { SectionHeader } from '@/components';
 import { NightLifeType } from '@/types';
 import { paths } from '@/utils';
-import barImg from '@images/bar-banner.jpg';
+import barImg from '@images/bar-banner.webp';
+import clubImg from '@images/club-banner.jpg';
+import loungeImg from '@images/lounge-banner.jpg';
+import otherImg from '@images/nightlife-banner.jpg';
 import { Card, CardFooter, Image } from '@nextui-org/react';
 import Link from 'next/link';
 
@@ -9,9 +12,9 @@ const NightlifeCategories = () => {
   return (
     <section className="flex flex-col gap-4">
       <SectionHeader title="Nightlife Categories" desc="From bars to lounges, we have it all" />
-      <div className="gap-2 grid grid-cols-3 grid-rows-2">
-        <div className="col-span-1 h-[300px]">
-          <Link href={paths.searchStay([NightLifeType.BAR])}>
+      <div className="gap-2 grid grid-cols-6 grid-rows-2">
+        <div className="col-span-2 h-[300px]">
+          <Link href={paths.searchNightlife([NightLifeType.BAR])}>
             <Card isPressable className="w-full h-full">
               <Image
                 isZoomed
@@ -21,7 +24,55 @@ const NightlifeCategories = () => {
                 src={barImg.src}
               />
               <CardFooter className="absolute bg-gradient-to-t from-black/60 to-transparent pt-4 bottom-0 z-10">
-                <h4 className="text-white font-medium text-3xl">Bar</h4>
+                <h4 className="text-white font-medium text-3xl">Bars</h4>
+              </CardFooter>
+            </Card>
+          </Link>
+        </div>
+        <div className="col-span-4 h-[300px]">
+          <Link href={paths.searchNightlife([NightLifeType.CLUB])}>
+            <Card isPressable className="w-full h-full">
+              <Image
+                isZoomed
+                removeWrapper
+                alt="Card background"
+                className="z-0 w-full h-full object-cover"
+                src={clubImg.src}
+              />
+              <CardFooter className="absolute bg-gradient-to-t from-black/60 to-transparent pt-4 bottom-0 z-10">
+                <h4 className="text-white font-medium text-3xl">Clubs</h4>
+              </CardFooter>
+            </Card>
+          </Link>
+        </div>
+        <div className="col-span-4 h-[300px]">
+          <Link href={paths.searchNightlife([NightLifeType.LOUNGE])}>
+            <Card isPressable className="w-full h-full">
+              <Image
+                isZoomed
+                removeWrapper
+                alt="Card background"
+                className="z-0 w-full h-full object-cover"
+                src={loungeImg.src}
+              />
+              <CardFooter className="absolute bg-gradient-to-t from-black/60 to-transparent pt-4 bottom-0 z-10">
+                <h4 className="text-white font-medium text-3xl">Lounges</h4>
+              </CardFooter>
+            </Card>
+          </Link>
+        </div>
+        <div className="col-span-2 h-[300px]">
+          <Link href={paths.searchNightlife([NightLifeType.OTHER])}>
+            <Card isPressable className="w-full h-full">
+              <Image
+                isZoomed
+                removeWrapper
+                alt="Card background"
+                className="z-0 w-full h-full object-cover"
+                src={otherImg.src}
+              />
+              <CardFooter className="absolute bg-gradient-to-t from-black/60 to-transparent pt-4 bottom-0 z-10">
+                <h4 className="text-white font-medium text-3xl">Others</h4>
               </CardFooter>
             </Card>
           </Link>
