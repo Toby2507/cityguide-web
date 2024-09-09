a8f941b7c22322063952800494da3f3a298ad3b9
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-09 07:27:07 +0100

    Update the UI and implementation of the user details section of the restaurant reservation page

--- src/containers/reservation/restaurant/user-detail.tsx
+++ src/containers/reservation/restaurant/user-detail.tsx
@@ -1,21 +1,24 @@
 'use client';
 
-import reservation from '@/containers/admin/home/reservation';
 import { useReservationStore } from '@/providers';
 import { IPartner, IRestaurant, IUserDetails } from '@/types';
-import { paths } from '@/utils';
-import { user, User, RadioGroup, Radio, Input } from '@nextui-org/react';
+import { generateTimeRange, paths } from '@/utils';
+import { Button, Input, Radio, RadioGroup, Select, SelectItem, Textarea, User } from '@nextui-org/react';
 import dayjs from 'dayjs';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { PiUserLight } from 'react-icons/pi';
+import { usePathname, useRouter } from 'next/navigation';
+import { useMemo } from 'react';
+import toast from 'react-hot-toast';
+import { PiCheckCircle, PiUserLight } from 'react-icons/pi';
+import { SlArrowRight } from 'react-icons/sl';
 
 interface Props {
   user: IUserDetails | null;
   restaurant: IRestaurant;
 }
 
-const RestaurantUserDetailReservation = ({ restaurant: { partner }, user }: Props) => {
+const RestaurantUserDetailReservation = ({ restaurant: { availability, partner }, user }: Props) => {
+  const { push } = useRouter();
   const pathname = usePathname();
   const { reservation, setReservation } = useReservationStore();
   const userDetails = [
@@ -27,6 +30,37 @@ const RestaurantUserDetailReservation = ({ restaurant: { partner }, user }: Prop
   const cancellation = (partner as IPartner).cancellationPolicy;
   const cancellationDeadline =
     cancellation && dayjs(reservation?.checkInDay).subtract(cancellation.daysFromReservation - 1, 'd');
+  const goodToKnow = useMemo(
+    () => [
+      ...(!cancellation
+        ? ['Stay flexible: You can cancel for free anytime']
+        : dayjs(cancellationDeadline).isBefore(dayjs())
+        ? []
+        : [
+            `Stay flexible: You can cancel for free before ${
+              dayjs(cancellationDeadline).isSame(dayjs())
+                ? 'today'
+                : dayjs(cancellationDeadline).format('ddd, MMM DD, YYYY')
+            }`,
+          ]),
+      "You'll get the entire table to your self",
+    ],
+    [cancellation, cancellationDeadline]
+  );
+  const timeRange = availability.find((a) => a.day === dayjs(reservation?.checkInDay).format('dddd'));
+  const times = timeRange
+    ? generateTimeRange(timeRange.from, dayjs(`2000-01-01 ${timeRange.to}`).subtract(1, 'h').format('HH:mm'))
+    : [];
+
+  const setReservationtime = (time: string) => {
+    const outTime = dayjs(`2000-01-01 ${time}`).add(1, 'h').format('HH:mm');
+    setReservation({ checkInTime: time, checkOutTime: outTime });
+  };
+  const goToFinal = () => {
+    if (!reservation?.checkInTime) return toast.error('Please select your estimated check-in time');
+    if (!reservation?.checkOutTime) return toast.error('Please select your estimated check-out time');
+    push(`${pathname}/complete`);
+  };
   return (
     <section className="flex flex-col gap-2">
       <article className="flex flex-col gap-3 border-2 rounded-xl px-6 py-4">
@@ -111,6 +145,69 @@ const RestaurantUserDetailReservation = ({ restaurant: { partner }, user }: Prop
           ) : null}
         </div>
       </article>
+      <article className="flex flex-col gap-2 border-2 rounded-xl px-6 py-4">
+        <h3 className="text-lg font-bold tracking-wide">Good to know</h3>
+        <div className="flex flex-col gap-1">
+          {goodToKnow.map((info) => (
+            <div className="flex items-center gap-2" key={info}>
+              <PiCheckCircle className="text-green-600" size={24} />
+              <p className="text-sm font-light">{info}</p>
+            </div>
+          ))}
+        </div>
+      </article>
+      <article className="flex flex-col gap-2 border-2 rounded-xl px-6 py-4 pb-8">
+        <h3 className="text-lg font-bold tracking-wide">Your arrival time</h3>
+        <div className="flex items-center gap-2">
+          <PiCheckCircle className="text-green-600" size={26} />
+          <p className="text-sm">
+            Your table(s) would be ready from {dayjs(`2000-01-01 ${reservation?.checkInTime}`).format('hh:mm A')} to{' '}
+            {dayjs(`2000-01-01 ${reservation?.checkOutTime}`).format('hh:mm A')}
+          </p>
+        </div>
+        <div className="grid grid-cols-2 gap-4 pt-2">
+          <Select
+            selectedKeys={[reservation?.checkInTime || '']}
+            onChange={(e) => setReservationtime(e.target.value)}
+            isRequired
+            label="Add your estimated arrival time"
+            placeholder=" "
+          >
+            {times.map(({ key, label }) => (
+              <SelectItem key={key}>{label}</SelectItem>
+            ))}
+          </Select>
+        </div>
+      </article>
+      <article className="flex flex-col gap-2 border-2 rounded-xl px-6 py-4 pb-8">
+        <h3 className="text-lg font-bold tracking-wide">Special request</h3>
+        <p className="text-xs font-medium">
+          Special request can’t be granted, but the property will do it’s best to meet your needs. You can always make a
+          special request after your booking is completed.
+        </p>
+        <div className="flex flex-col gap-2 pt-3">
+          <p className="text-sm font-semibold">
+            Please write your request in English <span className="text-xs text-accentGray font-medium">(optional)</span>
+          </p>
+          <Textarea
+            value={reservation?.specialRequest || ''}
+            onValueChange={(specialRequest) => setReservation({ specialRequest })}
+            name="special request"
+            radius="full"
+            variant="bordered"
+          />
+        </div>
+      </article>
+      <Button
+        className="self-end mt-2 w-fit"
+        color="primary"
+        endContent={<SlArrowRight />}
+        onPress={goToFinal}
+        radius="sm"
+        size="lg"
+      >
+        Next: Final details
+      </Button>
     </section>
   );
 };
