5230adfdfca58daa351f570e829a0a4d8dd8fab6
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-10 15:27:33 +0100

    Pass the demo reservations into the reservation table on the admin dashboard pending other dashboard analytics

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,10 +1,11 @@
 import { AdminDashboard, AdminReservation } from '@/containers';
+import { reservations } from '@/data';
 
 const AdminDashboardPage = async () => {
   return (
     <div className="flex flex-col gap-6 pb-20">
       <AdminDashboard />
-      <AdminReservation />
+      <AdminReservation reservations={reservations} />
     </div>
   );
 };
--- src/data/index.ts
+++ src/data/index.ts
@@ -3,5 +3,6 @@ export * from './amenities';
 export * from './core';
 export * from './create-property';
 export * from './demo-locations';
+export * from './demo-models';
 export * from './demo-services';
 export * from './list-property';
