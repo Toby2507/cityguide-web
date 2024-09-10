f15c46d1552818d41c1a78552d9ee7a9632eddd2
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-10 15:03:58 +0100

    Add a demo airtime page for the time being that can handle navigation

---
+++ src/app/airtime/page.tsx
@@ -0,0 +1,22 @@
+import { Destinations, DiscoverHome, Footer, HomeHeader, StaySearchBar, SubscribeBox } from '@/containers';
+
+const AirtimePage = () => {
+  return (
+    <>
+      <HomeHeader />
+      <main className="flex flex-col gap-36 bg-white">
+        <div className="container mx-auto px-10 flex flex-col gap-20 max-w-7xl">
+          <StaySearchBar extraClass="-mt-9" isMain />
+        </div>
+        <DiscoverHome />
+        <div className="container mx-auto px-10 flex flex-col gap-20 max-w-7xl">
+          <Destinations />
+        </div>
+        <SubscribeBox />
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default AirtimePage;
