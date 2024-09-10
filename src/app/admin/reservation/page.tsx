f1898500025ac9c4f809210c3fec504385a38941
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-10 07:17:24 +0100

    Update the reservation page to fetch partner's reservations

--- src/app/admin/reservation/page.tsx
+++ src/app/admin/reservation/page.tsx
@@ -1,7 +1,15 @@
 import { AdminReservation } from '@/containers';
+import { getPartnerReservation } from '@/server';
+import toast from 'react-hot-toast';
 
-const AdminReservationsPage = () => {
-  return <AdminReservation />;
+const AdminReservationsPage = async () => {
+  try {
+    const reservations = await getPartnerReservation();
+    return <AdminReservation reservations={reservations} />;
+  } catch (err: any) {
+    toast.error(err.message);
+    return null;
+  }
 };
 
 export default AdminReservationsPage;
