5109c67fb1ef0e9ef654ffbb2dc6fc6cda2e6d49
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-14 15:47:03 +0100

    Update the reservation detail page property component

--- src/components/reservations/reservation-property.tsx
+++ src/components/reservations/reservation-property.tsx
@@ -1,34 +1,44 @@
 'use client';
 
 import { INightLife, IReservation, IRestaurant, IStay } from '@/types';
-import { Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from '@nextui-org/react';
+import { formatAddress } from '@/utils';
+import { Chip, Image } from '@nextui-org/react';
+import RatingCard from '../common/rating-card';
 
 interface Props {
   property: IStay | IRestaurant | INightLife;
   reservation: IReservation;
 }
 
-const ReservationProperty = ({ property: { address, name } }: Props) => {
+const ReservationProperty = ({ property, reservation }: Props) => {
+  const { address, avatar, name, rating, reviewCount, summary } = property;
+  const { propertyType } = reservation;
+  const type = (property as IStay).type || (property as IRestaurant).priceRange;
   return (
-    <section className="col-span-9 flex flex-col gap-4 border-r p-6">
-      <h2 className="text-lg font-semibold">Property Details</h2>
-      <Table hideHeader removeWrapper aria-label="property details">
-        <TableHeader>
-          <TableColumn>Rule</TableColumn>
-          <TableColumn>Description</TableColumn>
-        </TableHeader>
-        <TableBody>
-          <TableRow>
-            <TableCell className="flex items-center gap-2 font-medium text-accentGray">Property name</TableCell>
-            <TableCell className="w-10/12 font-semibold">{name}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="flex items-center gap-2 font-medium text-accentGray">Property location</TableCell>
-            <TableCell className="w-10/12 font-semibold">{address.fullAddress}</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </section>
+    <article className="grid grid-cols-10 gap-4 border rounded-3xl p-3">
+      <figure className="h-full col-span-2 w-full">
+        <Image
+          src={avatar}
+          width="full"
+          alt={name}
+          radius="none"
+          className="w-full object-cover rounded-2xl aspect-square"
+        />
+      </figure>
+      <div className="col-span-8 flex flex-col gap-2">
+        <Chip size="sm" className="text-sm" color="primary" radius="sm" variant="flat">
+          <span className="text-xs font-medium tracking-wider">{type}</span>
+        </Chip>
+        <header className="flex justify-between gap-10 w-full">
+          <div className="flex flex-col gap-1">
+            <h3 className="text-xl font-semibold tracking-wide">{name}</h3>
+            <p className="text-xs text-accentGray underline">{formatAddress(address)}</p>
+          </div>
+          <RatingCard rating={rating} reviewCount={reviewCount} size="sm" />
+        </header>
+        <p className="text-sm">{summary.split('\n')[0]}</p>
+      </div>
+    </article>
   );
 };
 
