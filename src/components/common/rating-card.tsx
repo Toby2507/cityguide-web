a6b191d477e7a0ef9cacdcb49ab69ed7910ab328
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-14 15:46:09 +0100

    Add the bg and text color prop to the rating card

--- src/components/common/rating-card.tsx
+++ src/components/common/rating-card.tsx
@@ -4,15 +4,17 @@ interface Props {
   rating: number;
   reviewCount: number;
   reversed?: boolean;
+  bgColor?: string;
+  textColor?: string;
   size?: 'sm' | 'md';
 }
 
-const RatingCard = ({ rating, reviewCount, reversed, size = 'md' }: Props) => {
+const RatingCard = ({ rating, reviewCount, reversed, textColor, bgColor, size = 'md' }: Props) => {
   if (!reviewCount) return null;
   return (
     <div className="flex items-center justify-end gap-2 min-w-fit">
       <div className={`flex flex-col ${reversed ? 'items-start' : 'items-end'}`}>
-        <p className={`flex-1 text-primary ${size === 'md' ? 'text-base' : 'text-sm'} font-bold`}>
+        <p className={`flex-1 ${textColor ?? 'text-primary'} ${size === 'md' ? 'text-base' : 'text-sm'} font-bold`}>
           {ratingRank(rating, reviewCount)}
         </p>
         <p className={`${size === 'md' ? 'text-xs' : 'text-[10px]'} text-accentGray font-medium`}>
@@ -20,9 +22,9 @@ const RatingCard = ({ rating, reviewCount, reversed, size = 'md' }: Props) => {
         </p>
       </div>
       <p
-        className={`bg-primary rounded-t-lg text-white ${size === 'md' ? 'text-xl p-3' : 'text-base p-2'} ${
-          reversed ? 'order-first rounded-ee-lg' : 'rounded-es-lg'
-        }`}
+        className={`${bgColor ?? 'bg-primary'} rounded-t-lg text-white ${
+          size === 'md' ? 'text-xl p-3' : 'text-base p-2'
+        } ${reversed ? 'order-first rounded-ee-lg' : 'rounded-es-lg'}`}
       >
         {rating.toFixed(1)}
       </p>
