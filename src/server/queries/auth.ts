61d9263b01ff2797d7fa8103d1ce8d73deecc9af
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-14 15:42:33 +0100

    Add the refresh access token function

--- src/server/queries/auth.ts
+++ src/server/queries/auth.ts
@@ -1,8 +1,9 @@
 'use server';
 
 import { IUser } from '@/types';
-import { fetchWithReAuth } from '@/utils';
-import { _logout } from '../actions/cookie';
+import { fetchBaseQuery, fetchWithReAuth, paths } from '@/utils';
+import { redirect } from 'next/navigation';
+import { _logout, getCookie, setCookie } from '../actions/cookie';
 
 export const getUserProfile = async () => {
   const res = await fetchWithReAuth('user/profile', { method: 'GET' });
@@ -13,4 +14,19 @@ export const getUserProfile = async () => {
 
 export const logout = async () => {
   await _logout();
+  redirect(paths.login());
+};
+
+export const refreshAccessToken = async () => {
+  const refreshToken = await getCookie('refresh-token');
+  const res = await fetchBaseQuery('account/refreshaccess', {
+    method: 'POST',
+    body: JSON.stringify({ refreshToken }),
+  });
+  if (res.ok) {
+    const { accessToken } = await res.json();
+    await setCookie('access-token', accessToken);
+    return true;
+  }
+  return false;
 };
