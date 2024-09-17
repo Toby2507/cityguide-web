92529f1500afe170beaf8d4dda4d78665b282d0a
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-17 12:37:18 +0100

    Add a custom hook to refresh auth status after auth related actions

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,1 +1,2 @@
+export { default as useAuthRefresh } from './useAuthRefresh';
 export { default as useCustomImageSelect } from './useCustomImageSelect';
---
+++ src/hooks/useAuthRefresh.ts
@@ -0,0 +1,19 @@
+import { useAuth } from '@/providers';
+import { logout } from '@/server';
+
+const useAuthRefresh = () => {
+  const { refreshAuth } = useAuth();
+
+  const refreshAfterAction = async (action: Function) => {
+    await action();
+    await refreshAuth();
+  };
+  const refreshAfterLogout = async () => {
+    await logout();
+    await refreshAuth();
+  };
+
+  return { refreshAfterAction, refreshAfterLogout };
+};
+
+export default useAuthRefresh;
