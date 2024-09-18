0c330b5c2b1ccfe6ddda7b7df50a0d7f5dbc0c0d
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-18 12:51:55 +0100

    Transition the restaurant home page to use react-query with ssr

--- src/app/restaurant/page.tsx
+++ src/app/restaurant/page.tsx
@@ -9,10 +9,19 @@ import {
   SubscribeBox,
   TopOffers,
 } from '@/containers';
+import { getTrendingRestaurants } from '@/server';
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 
-const RestaurantHomePage = () => {
+const RestaurantHomePage = async () => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['trending-restaurants'],
+    queryFn: getTrendingRestaurants,
+  });
+
   return (
-    <>
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <HomeHeader />
       <main className="flex flex-col gap-36 bg-white">
         <div className="container mx-auto px-10 flex flex-col gap-20 max-w-7xl">
@@ -28,7 +37,7 @@ const RestaurantHomePage = () => {
         <SubscribeBox />
       </main>
       <Footer />
-    </>
+    </HydrationBoundary>
   );
 };
 
--- src/containers/home/restaurant/trending-destinations.tsx
+++ src/containers/home/restaurant/trending-destinations.tsx
@@ -1,27 +1,37 @@
-import { ErrorDisplay, PlaceCard, SectionHeader } from '@/components';
+'use client';
+
+import { PlaceCard, SectionHeader } from '@/components';
 import { getTrendingRestaurants } from '@/server';
 import { paths } from '@/utils';
+import { useQuery } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
 
-const RestaurantTrendingDestination = async () => {
-  try {
-    const restaurants = await getTrendingRestaurants();
-    return (
-      <section className="flex flex-col gap-4">
-        <SectionHeader
-          title="Trending restaurants"
-          desc="Most popular choices for travelers from Nigeria"
-          viewUrl={restaurants.length > 6 ? '#' : ''}
-        />
-        <div className="grid grid-cols-3 gap-4 px-2">
-          {restaurants?.slice(0, 6).map((place) => (
-            <PlaceCard key={place._id} {...place} refPath={paths.restaurantDetail} />
-          ))}
-        </div>
-      </section>
-    );
-  } catch (err: any) {
-    return <ErrorDisplay error={err.message} />;
-  }
+const RestaurantTrendingDestination = () => {
+  const {
+    data: restaurants,
+    isPending,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ['trending-restaurants'],
+    queryFn: getTrendingRestaurants,
+  });
+  if (isPending) return null;
+  if (isError) return toast.error(error.message);
+  return (
+    <section className="flex flex-col gap-4">
+      <SectionHeader
+        title="Trending restaurants"
+        desc="Most popular choices for travelers from Nigeria"
+        viewUrl={restaurants.length > 6 ? '#' : ''}
+      />
+      <div className="grid grid-cols-3 gap-4 px-2">
+        {restaurants?.slice(0, 6).map((place) => (
+          <PlaceCard key={place._id} {...place} refPath={paths.restaurantDetail} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default RestaurantTrendingDestination;
