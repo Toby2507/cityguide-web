f0680d2db4ad188a72180242ef9204ac083a9eca
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-18 10:11:33 +0100

    Add the react query provider

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -2,3 +2,4 @@ export * from './auth.provider';
 export * from './reservation-provider';
 export * from './search-provider';
 export * from './socket.provider';
+export * from './react-query.provider';
