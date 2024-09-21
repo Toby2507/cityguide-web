50910438e95ccae0086f9684cf91b3b37a6b1280
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-21 08:42:04 +0100

    Add the socket listeners and query invalidators

--- src/providers/socket.provider.tsx
+++ src/providers/socket.provider.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { TSocket } from '@/types';
+import { useQueryClient } from '@tanstack/react-query';
 import { createContext, ReactNode, useCallback, useContext, useEffect, useRef, useState } from 'react';
 import { io } from 'socket.io-client';
 import { useAuth } from './auth.provider';
@@ -16,6 +17,7 @@ interface ISocketContext {
 const SocketContext = createContext<ISocketContext>({ socket: null, isConnected: false });
 export const SocketProvider = ({ children }: ISocketProvider) => {
   const socketRef = useRef<TSocket | null>(null);
+  const queryClient = useQueryClient();
   const [isConnected, setIsConnected] = useState<boolean>(false);
   const { user } = useAuth();
 
@@ -29,7 +31,26 @@ export const SocketProvider = ({ children }: ISocketProvider) => {
     socket.on('disconnect', () => {
       setIsConnected(false);
     });
+    socket.on('restaurant_menu', ({ id }) => {
+      queryClient.invalidateQueries({ queryKey: ['restaurant', id] });
+      queryClient.invalidateQueries({ queryKey: ['trending-restaurants'] });
+    });
+    socket.on('stay_acc', ({ id }) => {
+      queryClient.invalidateQueries({ queryKey: ['stay', id] });
+      queryClient.invalidateQueries({ queryKey: ['trending-stays'] });
+    });
+    socket.on('update_property', ({ id, type }) => {
+      queryClient.invalidateQueries({ queryKey: [type.toLowerCase(), id] });
+    });
+    socket.on('delete_property', ({ id, type }) => {
+      queryClient.invalidateQueries({ queryKey: [type.toLowerCase(), id] });
+      queryClient.invalidateQueries({ queryKey: [`trending-${type.toLowerCase()}s`] });
+    });
+    socket.on('new_reservation', () => {
+      queryClient.invalidateQueries({ queryKey: ['reservations', 'admin'] });
+    });
     socketRef.current = socket;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user?.id]);
 
   useEffect(() => {
