e12bb46aee3656eea9df72db9aba934c685c543a
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-22 05:08:20 +0100

    Add the implementation for the property store set properties functions

--- src/services/property-store.ts
+++ src/services/property-store.ts
@@ -2,6 +2,8 @@ import { IPropertyStore } from '@/types';
 import { createStore } from 'zustand';
 import { persist } from 'zustand/middleware';
 
+const initialState = { step: 1, topStep: 1, property: {} };
+
 export const propertyStore = () => {
   return createStore<IPropertyStore>()(
     persist(
@@ -9,9 +11,27 @@ export const propertyStore = () => {
         stay: null,
         restaurant: null,
         nightlife: null,
-        setStay(info) {},
-        setRestaurant(info) {},
-        setNightlife(info) {},
+        setStay(info) {
+          return set((state) => {
+            if (!info) return { stay: null };
+            if (!state.stay) return { stay: { ...initialState, ...info } };
+            return { stay: { ...state.stay, ...info } };
+          });
+        },
+        setRestaurant(info) {
+          return set((state) => {
+            if (!info) return { restaurant: null };
+            if (!state.restaurant) return { restaurant: { ...initialState, ...info } };
+            return { restaurant: { ...state.restaurant, ...info } };
+          });
+        },
+        setNightlife(info) {
+          return set((state) => {
+            if (!info) return { nightlife: null };
+            if (!state.nightlife) return { nightlife: { ...initialState, ...info } };
+            return { nightlife: { ...state.nightlife, ...info } };
+          });
+        },
       }),
       { name: 'cityguide-property' }
     )
