b1d26746136e1a7c7b35c2e6e297f90146d8769d
Toby2507 <oluwatobisalau2000@gmail.com>
2024-10-03 14:27:04 +0100

    Add the handle read notifications functions and update the notifications to show the most recent notifications first

--- src/containers/common/notification.tsx
+++ src/containers/common/notification.tsx
@@ -1,8 +1,11 @@
 'use client';
 
-import { getNotifications } from '@/server';
+import { NotificationItem } from '@/components';
+import { getNotifications, readNotifications } from '@/server';
 import { Badge, Button, Popover, PopoverContent, PopoverTrigger, Spinner } from '@nextui-org/react';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useMemo } from 'react';
+import toast from 'react-hot-toast';
 import { FaBell } from 'react-icons/fa6';
 
 const Notification = () => {
@@ -11,11 +14,24 @@ const Notification = () => {
     queryFn: async () => await getNotifications(),
     staleTime: 60 * 10 * 1000,
   });
-
+  const queryClient = useQueryClient();
+  const unreadNotifications = useMemo(() => notifications?.filter((n) => !n.isRead).map((n) => n._id), [notifications]);
   if (!notifications?.length || isPending) return null;
-  console.log({ notifications });
+
+  const handleReadNotifications = async () => {
+    if (!unreadNotifications?.length) return;
+    try {
+      await readNotifications(unreadNotifications);
+      queryClient.invalidateQueries({ queryKey: ['notifications'] });
+    } catch (err: any) {
+      toast.error(err.message);
+    }
+  };
+  const onOpenChange = (open: boolean) => {
+    if (!open) handleReadNotifications();
+  };
   return (
-    <Popover offset={20} placement="bottom">
+    <Popover offset={20} onOpenChange={onOpenChange} placement="bottom">
       <PopoverTrigger>
         <Button aria-label="notifications" isIconOnly radius="sm" variant="light">
           <Badge content="" size="sm" color="danger">
@@ -25,15 +41,21 @@ const Notification = () => {
       </PopoverTrigger>
       <PopoverContent>
         {(titleProps) => (
-          <div className="flex flex-col gap-2 max-w-7xl h-[70vh] p-0">
+          <div className="flex flex-col max-w-xl h-[70vh] p-0">
             <div className="flex items-center justify-between border-b gap-32 p-2">
-              <h1 className="text-xl font-semibold tracking-wide">Notifications</h1>
-              <Button color="primary" size="sm" variant="light">
+              <h1 className="text-xl font-semibold tracking-wide" {...titleProps}>
+                Notifications
+              </h1>
+              <Button onPress={handleReadNotifications} color="primary" size="sm" variant="light">
                 Mark all as read
               </Button>
             </div>
-            <div className="flex-1 grid place-items-center gap-2 p-2 pb-4">
-              {isPending ? <Spinner size="lg" /> : null}
+            <div className="flex-1 grid gap-2 px-2 py-4 overflow-auto">
+              {isPending ? (
+                <Spinner size="lg" />
+              ) : (
+                notifications.map((notification) => <NotificationItem key={notification._id} {...notification} />)
+              )}
             </div>
           </div>
         )}
