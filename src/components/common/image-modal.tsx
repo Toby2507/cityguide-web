9482745f833052389e6fdfc943b115bae4462907
Toby2507 <oluwatobisalau2000@gmail.com>
2024-10-07 15:08:31 +0100

    Remove the noBook prop since a separate modal was created for the apartments

--- src/components/common/image-modal.tsx
+++ src/components/common/image-modal.tsx
@@ -15,11 +15,10 @@ interface Props {
   avatar: string;
   accommodation?: IAccommodation[];
   hotelRating?: HotelRating;
-  noBook?: boolean;
   onOpenChange: () => void;
 }
 
-const ImageModal = ({ isOpen, onOpenChange, name, noBook, hotelRating, avatar, images, accommodation }: Props) => {
+const ImageModal = ({ isOpen, onOpenChange, name, hotelRating, avatar, images, accommodation }: Props) => {
   const [activeTab, setActiveTab] = useState<string>('General');
   const [viewType, setViewType] = useState<string>('grid');
   const [activeImage, setActiveImage] = useState<number>(0);
@@ -45,112 +44,108 @@ const ImageModal = ({ isOpen, onOpenChange, name, noBook, hotelRating, avatar, i
     <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="bottom" size="5xl" scrollBehavior="inside">
       <ModalContent className="h-[90vh]">
         {(onClose) => (
-          <>
-            <ModalBody>
-              <div className="relative flex items-center justify-center gap-2 border-b py-2">
-                <h3 className="text-xl font-semibold tracking-wide">{name}</h3>
-                {hotelRating && <CustomStars value={hotelRating} />}
-                {!noBook && (
-                  <Link href="#availability">
-                    <Button color="primary" className="px-10 font-semibold" radius="sm" size="sm" onPress={onClose}>
-                      Book now
-                    </Button>
-                  </Link>
-                )}
-                {viewType === 'cinema' && (
-                  <Button
-                    className="absolute left-0 pr-4 gap-0"
-                    size="sm"
-                    color="primary"
-                    variant="light"
-                    startContent={<RxCaretLeft size={20} />}
-                    onPress={() => setViewType('grid')}
-                  >
-                    Gallery
-                  </Button>
-                )}
-              </div>
-              {viewType === 'grid' ? (
-                <>
-                  <div className="flex items-center gap-4">
-                    {tabs.map(({ name, avatar }) => (
-                      <div className="flex flex-col items-center gap-1" key={name}>
-                        <figure
-                          className={`h-20 rounded-lg overflow-hidden cursor-pointer p-[1px] border-2 ${
-                            activeTab === name ? 'border-primary' : 'border-transparent'
-                          }`}
-                        >
-                          <Image
-                            src={avatar}
-                            width="full"
-                            alt="stay"
-                            height={48}
-                            radius="none"
-                            isZoomed
-                            removeWrapper
-                            className="object-cover h-full w-full rounded-md"
-                            onClick={() => setActiveTab(name)}
-                          />
-                        </figure>
-                        <p className="text-xs font-medium">{name}</p>
-                      </div>
-                    ))}
-                  </div>
-                  <div className="grid grid-cols-4 content-start gap-2">
-                    {currentImages?.map((image, idx) => (
-                      <figure key={idx} className="w-full rounded-md overflow-hidden cursor-pointer">
+          <ModalBody>
+            <div className="relative flex items-center justify-center gap-2 border-b py-2">
+              <h3 className="text-xl font-semibold tracking-wide">{name}</h3>
+              {hotelRating && <CustomStars value={hotelRating} />}
+              <Link href="#availability">
+                <Button color="primary" className="px-10 font-semibold" radius="sm" size="sm" onPress={onClose}>
+                  Book now
+                </Button>
+              </Link>
+              {viewType === 'cinema' && (
+                <Button
+                  className="absolute left-0 pr-4 gap-0"
+                  size="sm"
+                  color="primary"
+                  variant="light"
+                  startContent={<RxCaretLeft size={20} />}
+                  onPress={() => setViewType('grid')}
+                >
+                  Gallery
+                </Button>
+              )}
+            </div>
+            {viewType === 'grid' ? (
+              <>
+                <div className="flex items-center gap-4">
+                  {tabs.map(({ name, avatar }) => (
+                    <div className="flex flex-col items-center gap-1" key={name}>
+                      <figure
+                        className={`h-20 rounded-lg overflow-hidden cursor-pointer p-[1px] border-2 ${
+                          activeTab === name ? 'border-primary' : 'border-transparent'
+                        }`}
+                      >
                         <Image
-                          src={image}
+                          src={avatar}
                           width="full"
                           alt="stay"
+                          height={48}
                           radius="none"
                           isZoomed
                           removeWrapper
-                          className="object-cover h-full w-full"
-                          onClick={() => viewImage(idx)}
+                          className="object-cover h-full w-full rounded-md"
+                          onClick={() => setActiveTab(name)}
                         />
                       </figure>
-                    ))}
-                  </div>
-                </>
-              ) : (
-                <div className="flex items-center justify-center h-full gap-6">
-                  <Button
-                    isDisabled={activeImage <= 0}
-                    color="default"
-                    isIconOnly
-                    radius="full"
-                    size="lg"
-                    variant="light"
-                    onPress={goBackward}
-                  >
-                    <PiCaretLeftBold size={40} />
-                  </Button>
-                  <figure className="w-full h-[70vh] rounded-md overflow-hidden cursor-pointer">
-                    <Image
-                      src={currentImages[activeImage]}
-                      width="full"
-                      alt="stay"
-                      radius="none"
-                      removeWrapper
-                      className="object-cover h-full w-full"
-                    />
-                  </figure>
-                  <Button
-                    isDisabled={activeImage >= currentImages.length - 1}
-                    color="default"
-                    isIconOnly
-                    size="lg"
-                    radius="full"
-                    variant="light"
-                    onPress={goForward}
-                  >
-                    <PiCaretRightBold size={40} />
-                  </Button>
+                      <p className="text-xs font-medium">{name}</p>
+                    </div>
+                  ))}
                 </div>
-              )}
-            </ModalBody>
-          </>
+                <div className="grid grid-cols-4 content-start gap-2">
+                  {currentImages?.map((image, idx) => (
+                    <figure key={idx} className="w-full rounded-md overflow-hidden cursor-pointer">
+                      <Image
+                        src={image}
+                        width="full"
+                        alt="stay"
+                        radius="none"
+                        isZoomed
+                        removeWrapper
+                        className="object-cover h-full w-full"
+                        onClick={() => viewImage(idx)}
+                      />
+                    </figure>
+                  ))}
+                </div>
+              </>
+            ) : (
+              <div className="flex items-center justify-center h-full gap-6">
+                <Button
+                  isDisabled={activeImage <= 0}
+                  color="default"
+                  isIconOnly
+                  radius="full"
+                  size="lg"
+                  variant="light"
+                  onPress={goBackward}
+                >
+                  <PiCaretLeftBold size={40} />
+                </Button>
+                <figure className="w-full h-[70vh] rounded-md overflow-hidden cursor-pointer">
+                  <Image
+                    src={currentImages[activeImage]}
+                    width="full"
+                    alt="stay"
+                    radius="none"
+                    removeWrapper
+                    className="object-cover h-full w-full"
+                  />
+                </figure>
+                <Button
+                  isDisabled={activeImage >= currentImages.length - 1}
+                  color="default"
+                  isIconOnly
+                  size="lg"
+                  radius="full"
+                  variant="light"
+                  onPress={goForward}
+                >
+                  <PiCaretRightBold size={40} />
+                </Button>
+              </div>
+            )}
+          </ModalBody>
         )}
       </ModalContent>
     </Modal>
