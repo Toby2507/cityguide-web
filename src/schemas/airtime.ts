d5bdf16f7f61e73f7585602a1e74d6add9a349b8
Toby2507 <oluwatobisalau2000@gmail.com>
2024-11-21 10:20:07 +0100

    Update the vtu related schemas

--- src/schemas/airtime.ts
+++ src/schemas/airtime.ts
@@ -1,16 +1,16 @@
-import { AirtimeNetworks, AirtimePurchaseTypes } from '@/types';
+import { ISPs, VTUType } from '@/types';
 import { nativeEnum, number, object, string, z } from 'zod';
 
-export const airtimeReceiverSchema = object({
-  firstName: string({ required_error: 'First name is required' }).min(3, 'First name requires atleast 3 characters'),
-  lastName: string({ required_error: 'Last name is required' }).min(3, 'Last name requires atleast 3 characters'),
+export const savedReceiverSchema = object({
+  firstName: string({ required_error: 'First name is required' }).min(1, 'First name should be atleast 1 character'),
+  lastName: string({ required_error: 'Last name is required' }).min(1, 'Last name should be atleast 1 character'),
   phoneNumber: string({ required_error: 'Phone number is required' }).regex(
     /^(?:(?:\+|0{1,2})[- )(?]*\d{0,3}[- )(?]*\d{3}[- )(?]*\d{3}[- )(?]*\d{4})$/,
     'Invalid phone number'
   ),
-  network: nativeEnum(AirtimeNetworks, {
-    required_error: 'Receiver network is required',
-    invalid_type_error: 'Network should be MTN | GLO | Airtel | 9Mobile',
+  network: nativeEnum(ISPs, {
+    required_error: 'Network is required',
+    invalid_type_error: 'Invalid network',
   }),
 });
 
@@ -26,14 +26,14 @@ export const airtimePurchaseSchema = object({
         /^(?:(?:\+|0{1,2})[- )(?]*\d{0,3}[- )(?]*\d{3}[- )(?]*\d{3}[- )(?]*\d{4})$/,
         'Invalid phone number'
       ),
-      network: nativeEnum(AirtimeNetworks, {
+      network: nativeEnum(ISPs, {
         required_error: 'Receiver network is required',
         invalid_type_error: 'Network should be MTN | GLO | Airtel | 9Mobile',
       }),
     },
     { required_error: 'Receiver object is required' }
   ),
-  type: nativeEnum(AirtimePurchaseTypes, {
+  type: nativeEnum(VTUType, {
     required_error: 'Purchase type is required',
     invalid_type_error: 'Purchase type should be VTU | Data',
   }),
