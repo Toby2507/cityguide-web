1445c1bca122b4d6d9c3ae9506f25ebf4f79f0c1
Toby2507 <oluwatobisalau2000@gmail.com>
2024-11-28 21:08:30 +0100

    Update the vtu purchase schema

--- src/schemas/airtime.ts
+++ src/schemas/airtime.ts
@@ -1,5 +1,5 @@
 import { ISPs, VTUType } from '@/types';
-import { nativeEnum, number, object, string, z } from 'zod';
+import { boolean, nativeEnum, number, object, string, z } from 'zod';
 
 export const savedReceiverSchema = object({
   firstName: string({ required_error: 'First name is required' }).min(1, 'First name should be atleast 1 character'),
@@ -14,34 +14,30 @@ export const savedReceiverSchema = object({
   }),
 });
 
-export const airtimePurchaseSchema = object({
-  receiver: object(
-    {
-      firstName: string({ required_error: 'First name is required' }).min(
-        3,
-        'First name requires atleast 3 characters'
-      ),
-      lastName: string({ required_error: 'Last name is required' }).min(3, 'Last name requires atleast 3 characters'),
-      phoneNumber: string({ required_error: 'Phone number is required' }).regex(
-        /^(?:(?:\+|0{1,2})[- )(?]*\d{0,3}[- )(?]*\d{3}[- )(?]*\d{3}[- )(?]*\d{4})$/,
-        'Invalid phone number'
-      ),
-      network: nativeEnum(ISPs, {
-        required_error: 'Receiver network is required',
-        invalid_type_error: 'Network should be MTN | GLO | Airtel | 9Mobile',
-      }),
-    },
-    { required_error: 'Receiver object is required' }
+export const vtuPurchaseSchema = object({
+  firstName: string({ required_error: 'First name is required' }).min(1, 'First name should be atleast 1 character'),
+  lastName: string({ required_error: 'Last name is required' }).min(1, 'Last name should be atleast 1 character'),
+  phoneNumber: string({ required_error: 'Phone number is required' }).min(
+    11,
+    'Phone number should be atleast 11 characters'
   ),
+  network: nativeEnum(ISPs, {
+    required_error: 'Network is required',
+    invalid_type_error: 'Invalid network',
+  }),
+  serviceId: string({ required_error: 'Service id is required' }),
+  amount: number({ required_error: 'Amount is required', invalid_type_error: 'Amount should be a number' }).min(1),
+  dataValue: string().optional(),
   type: nativeEnum(VTUType, {
-    required_error: 'Purchase type is required',
-    invalid_type_error: 'Purchase type should be VTU | Data',
+    required_error: 'Transaction type is required',
+    invalid_type_error: 'Invalid transaction type',
   }),
-  amount: number({
-    required_error: 'Purchase amount is required',
-    invalid_type_error: 'Purchase amount should be a number',
-  }),
-  dataId: string().optional(),
+  payReference: string({ required_error: 'Pay reference is required' }),
+  useSavedCard: boolean({ invalid_type_error: 'Save card should be true or false' }).optional().default(false),
+  saveCard: boolean({ invalid_type_error: 'Save card should be true or false' }).optional().default(true),
+}).refine((data) => (data.type === VTUType.DATA && data.dataValue) || data.type !== VTUType.DATA, {
+  message: 'dataValue field is required for data transactions',
+  path: ['dataValue'],
 });
 
-export type AirtimePurchaseType = z.infer<typeof airtimePurchaseSchema>;
+export type VtuPurchaseType = z.infer<typeof vtuPurchaseSchema>;
